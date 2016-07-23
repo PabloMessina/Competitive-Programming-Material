e4e01c682570a7d3551839baa1072a8600364ea8
PabloMessina <pamessina@uc.cl>
2016-07-23 12:31:46 -0400

    update

--- Graphs/DFS.cpp
+++ Graphs/DFS.cpp
@@ -151,18 +151,28 @@ void topoSort(vii & adj_list,  vi &indegree) {
 vi depth(N,-1);
 vi low(N);
 vii graph(N,vi());
+int rootChildren = 0;
 void dfs(int u, int p, int d) {
   depth[u] = d;
   low[u] = d;
   for(int v : graph[u]) {
     if (v == p) continue; // direct edge to parent is not back edge
     if (depth[v] == -1) {
+      if (p == -1 && ++rootChildren > 1) // root
+          printf("root = %d is articulation point\n", root);
+
       dfs(v, u, d + 1);
-      if (low[v] >= low[u]) {
+
+      if (low[v] >= depth[u] && p != -1)
         printf("u = %d is articulation point\n",  u);
-        if (low[v] > low[u])
-          printf("(u,v) = (%d, %d) is cut edge\n", u, v);
-      }
+
+      if (low[v] > depth[u])
+        printf("(u,v) = (%d, %d) is cut edge\n", u, v);
+
+      if (low[v] < low[u]) low[u] = low[v];
+      
+    } else if (depth[v] < low[u]) {
+      low[u] = depth[v];
     }
   }
 }
