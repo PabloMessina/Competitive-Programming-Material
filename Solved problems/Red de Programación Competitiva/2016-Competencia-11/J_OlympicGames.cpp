f92b27fb7d34d0c3af646fbd9d05b0677e1f361c
PabloMessina <pamessina@uc.cl>
2016-10-08 14:02:01 -0400

    update

--- Solved problems/Red de Programación Competitiva/2016-Competencia-11/J_OlympicGames.cpp
+++ Solved problems/Red de Programación Competitiva/2016-Competencia-11/J_OlympicGames.cpp
@@ -1,3 +1,5 @@
+// tags: Point Line Duality, Convex Hull, Fractions
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,6 +15,9 @@ struct Point {
 };
 bool operator<(const Point& a, const Point& b) {
   return a.x < b.x || (a.x == b.x && a.y < b.y);
+}
+bool operator==(const Point& a, const Point& b) {
+  return a.x == b.x && a.y == b.y;
 }
 
 // ---------
@@ -37,6 +42,12 @@ bool operator==(const Fraction& f1, const Fraction& f2) {
   return f1.a == f2.a && f1.b == f2.b;
 }
 
+struct Interval {
+  Fraction start;
+  Fraction end;
+  int id;
+};
+
 // ---------
 int N;
 ll skm[MAXN];
@@ -46,6 +57,7 @@ ll ftn[MAXN];
 Point dual_sk[MAXN];
 Point dual_ft[MAXN];
 bool golden[MAXN];
+bool ties[MAXN];
 
 // =======================
 // Convex-Hull fuctions
@@ -55,19 +67,31 @@ ll isLeft(Point& o, Point& a, Point& b) {
   return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
 }
 
+void remember_ties(Point* pts) {
+  rep (i, 1, N-1) {
+    Point& a = pts[i-1];
+    Point& b = pts[i];
+    if (a == b) {
+      ties[a.id] = true;
+      ties[b.id] = true;
+    }
+  }
+}
+
 /* Get lower hull of skill dual points. This matches skill upper envelope */
 vector<Point> sk_lower_hull() {
   // sort dual skill points
   sort(dual_sk, dual_sk + N);
+  // remember ties
+  remember_ties(dual_sk);
   // build lower hull
   int k = 0;
   vector<Point> lh(N);
   rep(i, 0, N-1) {
-    while(k >= 2 && isLeft(lh[k-2], lh[k-1], dual_sk[i]) < 0) k--;
-    // add dual point unless it's vertically above the previous one
-    if (k == 0 || !(lh[k-1].x == dual_sk[i].x && lh[k-1].y < dual_sk[i].y))
-      lh[k++] = dual_sk[i];
+    while(k >= 2 && isLeft(lh[k-2], lh[k-1], dual_sk[i]) <= 0) k--;
+    lh[k++] = dual_sk[i];
   }
+  if (k >= 2 && lh[k-1].x == lh[k-2].x) k--;
   lh.resize(k);
   return lh;
 }
@@ -76,56 +100,43 @@ vector<Point> sk_lower_hull() {
 vector<Point> ft_upper_hull() {
   // sort dual fatigue points
   sort(dual_ft, dual_ft + N);
+  // remember ties
+  remember_ties(dual_ft);
   // build upper hull
   int k = 0;
   vector<Point> uh(N);
   invrep(i, N-1, 0) {
-    while(k >= 2 && isLeft(uh[k-2], uh[k-1], dual_ft[i]) < 0) k--;
-    // add dual point unless it's vertically below the previos one
-    if (k == 0 || !(uh[k-1].x == dual_ft[i].x && uh[k-1].y > dual_ft[i].y))
-      uh[k++] = dual_ft[i];
+    while(k >= 2 && isLeft(uh[k-2], uh[k-1], dual_ft[i]) <= 0) k--;
+    uh[k++] = dual_ft[i];
   }
+  if (k >= 2 && uh[k-1].x == uh[k-2].x) k--;
   uh.resize(k);
   return uh;
 }
 
 // ==================================
 
-/* Get a map from intervals to athlete ids*/
-map<pair<Fraction,Fraction>,set<int>> get_intervals_map(vector<Point>& pts, ll* ms, ll* ns) {
+/* Get time intervals from dual points */
+vector<Interval> get_intervals(vector<Point>& pts, ll* ms, ll* ns) {
 
   Fraction prev_t(-INF, 1);
-  set<int> ids;
   int prev_id = -1;
-  int curr_id;
-  map<pair<Fraction,Fraction>,set<int>> intvmap;
+  vector<Interval> intvs;
 
   for (Point& p : pts) {
-    curr_id = p.id;
-    if (prev_id == -1) {
-      ids.insert(curr_id);
-    } else {
-      if (ms[curr_id] == ms[prev_id]) { // same line
-        ids.insert(curr_id);
-      } else {
-        Fraction t (
-          ns[curr_id] - ns[prev_id],
-          ms[prev_id] - ms[curr_id]
-        );
-        intvmap[std::make_pair(prev_t, t)].insert(ids.begin(), ids.end());
-        ids.clear();
-        ids.insert(curr_id);
-        prev_t = t;
-      }
+    if (prev_id != -1) {
+      Fraction t (
+        ns[p.id] - ns[prev_id],
+        ms[prev_id] - ms[p.id]
+      );
+      if (!ties[prev_id])
+        intvs.push_back({prev_t, t, prev_id});
+      prev_t = t;      
     }
-    prev_id = curr_id;
+    prev_id = p.id;
   }
-
-  Fraction end(INF, 1);
-  intvmap[std::make_pair(prev_t, end)].insert(ids.begin(), ids.end());
-
-  // return interval map
-  return intvmap;
+  intvs.push_back({prev_t, Fraction(INF, 1), prev_id});
+  return intvs;
 }
 
 Fraction TIME_ZERO(0, 1);
@@ -133,44 +144,32 @@ Fraction TIME_ZERO(0, 1);
 /* Count golden athletes by checking intersections between skill intervals
 and fatigue intervals. Intervals are swept linearly by shifting 2 pointers. */
 int count_golden(
-  map<pair<Fraction,Fraction>,set<int>>& sk_intvmap,
-  map<pair<Fraction,Fraction>,set<int>>& ft_intvmap) {
+  vector<Interval>& sk_intvs,
+  vector<Interval>& ft_intvs) {
 
   int count = 0;
-  memset(golden, false, sizeof(bool)*N);
+  int i = 0, j = 0;
 
-  auto start_kv2 = ft_intvmap.begin();
-  for (auto& kv1 : sk_intvmap) {
-    const Fraction& start1 = kv1.first.first;
-    const Fraction& end1 = kv1.first.second;
-    const set<int>& ids1 = kv1.second;
+  while (i < sk_intvs.size() && j < ft_intvs.size()) {
+    Interval& ski = sk_intvs[i];
+    Interval& fti = ft_intvs[j];
+    Fraction max_start = std::max(ski.start, fti.start);
+    Fraction min_end = std::min(ski.end, fti.end);
 
-    bool first = true;
+    if (max_start < min_end && TIME_ZERO < min_end && ski.id == fti.id && !golden[ski.id]) {
 
-    for (auto kv2 = start_kv2; kv2 != ft_intvmap.end(); ++kv2) {
-      const Fraction& start2 = kv2->first.first;
-      const Fraction& end2 = kv2->first.second;
-      const set<int>& ids2 = kv2->second;
+      // printf ("golden found!! id = %d\n", ski.id);
+      // printf ("\tsk_i: (%lf, %lf)\n", ski.start.to_double(), ski.end.to_double());
+      // printf ("\tft_i: (%lf, %lf)\n", fti.start.to_double(), fti.end.to_double());
+      // printf ("\tintersection: (%lf, %lf)\n", max_start.to_double(), min_end.to_double());
 
-      Fraction max_start = std::max(start1, start2);
-      Fraction min_end = std::min(end1, end2);
-
-      if (max_start <= min_end) {
-        if (first && end1 <= end2) {
-          first = false;
-          start_kv2 = kv2;
-        }
-        if (TIME_ZERO <= min_end) {
-          for (int id : ids1) {
-            if (golden[id]) continue;
-            if (ids2.find(id) != ids2.end()) {
-              golden[id] = true;
-              count++;
-            }
-          }
-        }
-      }
+      golden[ski.id] = true;
+      count++;
     }
+
+    if (ski.end < fti.end) ++i;
+    else if (fti.end < ski.end) ++j;
+    else ++i, ++j;
   }
 
   return count;
@@ -179,9 +178,8 @@ int count_golden(
 int main() {
   while (scanf("%d", &N) == 1) {
 
-    // === DEBUGGING ===
-    // puts("=====================================");
-    // printf("N = %d\n", N);
+    memset(golden, false, sizeof(bool)*N);
+    memset(ties, false, sizeof(bool)*N);
 
     // -------------------------------------------------------
     // read input, save slopes & intercepts, save dual points
@@ -196,51 +194,14 @@ int main() {
     vector<Point> sklh = sk_lower_hull();
     vector<Point> ftuh = ft_upper_hull();
 
-
-    // === DEBUGGING ===
-    // puts("----------");
-    // puts("skill lower hull:");
-    // for (Point& p : sklh) {
-    //   printf("{x = %lld, y = %lld, id = %d, m = %lld, n =%lld}\n", p.x, p.y, p.id, skm[p.id], skn[p.id]);
-    // }
-    // puts("----------");
-    // puts("fatigue upper hull:");
-    // for (Point& p : ftuh) {
-    //   printf("{x = %lld, y = %lld, id = %d, m = %lld, n =%lld}\n", p.x, p.y, p.id, ftm[p.id], ftn[p.id]);
-    // }
-
-
     // --------------------------
     // get interval maps
-    auto sk_intvmap = get_intervals_map(sklh, skm, skn);
-    auto ft_intvmap = get_intervals_map(ftuh, ftm, ftn);
-
-
-    // === DEBUGGING ===
-    // puts("---- sk_intervals -----");
-    // for (auto& kv : sk_intvmap) {
-    //   double start = kv.first.first.to_double();
-    //   double end = kv.first.second.to_double();
-    //   printf("\t(%lf, %lf) => [", start, end);
-    //   for (int id : kv.second) printf("%d, ", id);
-    //   puts("]");
-    //   puts("");
-    // }
-
-    // puts("---- ft_intervals -----");
-    // for (auto& kv : ft_intvmap) {
-    //   double start = kv.first.first.to_double();
-    //   double end = kv.first.second.to_double();
-    //   printf("\t(%lf, %lf) => [", start, end);
-    //   for (int id : kv.second) printf("%d, ", id);
-    //   puts("]");
-    //   puts("");
-    // }
-
+    vector<Interval> sk_intvs = get_intervals(sklh, skm, skn);
+    vector<Interval> ft_intvs = get_intervals(ftuh, ftm, ftn);
 
     // ------------------------
     // print golden count
-    printf("%d\n", count_golden(sk_intvmap, ft_intvmap));
+    printf("%d\n", count_golden(sk_intvs, ft_intvs));
   }
   return 0;
 }
