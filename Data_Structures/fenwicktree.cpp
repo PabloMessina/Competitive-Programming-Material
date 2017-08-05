60f9b008520c2c9f754ea2c149f79e3b3650f04d
PabloMessina <pamessina@uc.cl>
2017-08-05 06:08:48 -0400

    update

--- Data_Structures/fenwicktree.cpp
+++ Data_Structures/fenwicktree.cpp
@@ -1,20 +1,23 @@
-#define LSOne(s) (s & (-s))
 struct FenwickTree {
-  vector<int> ft;
-  FenwickTree(int n) { ft.assign(n+1, 0); }
-  int rsq(int b) {
-    int sum = 0;
-    for (; b; b -= LSOne(b)) sum += ft[b];
-    return sum;
-  }
-  int rsq(int a, int b) {
-    return rsq(b) - (a == 1 ? 0 : rsq(a-1));
-  }
-  void adjust(int k, int v) {
-    for (; k < ft.size(); k += LSOne(k)) ft[k] += v;
-  }
-  void range_adj(int i, int j, int v) {
-    adjust(i, v);
-    adjust(j+1, -v);
-  }
+	vector<int> ft;
+	FenwickTree(int n) { ft.assign(n+1, 0); }
+	// prefix sum query (sum in range 1 .. b)
+	int psq(int b) {
+		int sum = 0;
+		for (; b; b -= (b & -b)) sum += ft[b];
+		return sum;
+	}
+	// range sum query (sum in range a .. b)
+	int rsq(int a, int b) {
+		return rsq(b) - (a == 1 ? 0 : rsq(a-1));
+	}
+	// increment k'th value by v (and propagate)
+	void add(int k, int v) { 
+		for (; k < ft.size(); k += (k & -k)) ft[k] += v;
+	}
+	// increment range [i ... j] with v (and propagate)
+	void range_add(int i, int j, int v) {
+		add(i, v);
+		add(j+1, -v);
+	}
 };
--- Solved problems/UVA/13009_FenceTheVegetablesFail.cpp
+++ Solved problems/UVA/13009_FenceTheVegetablesFail.cpp
@@ -1,107 +1,130 @@
-// tags: range compression, range sum query, sweep line
+// tags: domain compression, sweep line, fenwick tree (range updates)
 #include <bits/stdc++.h>
-#include <tr1/unordered_map>
 using namespace std;
-
-#define rep(i,a,b) for(int i=a; i<=b; ++i)
-#define umap tr1::unordered_map
-#define MAXN 100000
-#define LSOne(s) (s & (-s))
-
+#define rep(i,a,b) for(int i = a; i <= b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+#define umap unordered_map
+#define uset unordered_set
+typedef vector<int> vi;
+typedef vector<vi> vvi;
 typedef long long int ll;
+typedef pair<int,int> ii;
+typedef tuple<int,int,int> iii;
+typedef pair<double, int> pdi;
+typedef pair<ll, int> lli;
+// -------------------------------
 
-int P,V;
-struct Point { int x,y; };
-Point plant_pts[MAXN];
-Point fence_pts[MAXN];
+int P, V;
 
-struct Segment { int val, ymin, ymax, x; };
-bool operator<(const Segment& a, const Segment& b) {
-  return a.x < b.x;
-}
+// define 2 structs: Plant and Vertical Segment
+// both inheriting from struct X, in order to use
+// polymorphism
+enum Type { PLANT, VSEGMENT };
+struct X { int x; Type type; }; // keep track of x coordinate and type
+struct Plant : X {
+	int y, val;
+	Plant(int x, int y, int val) {
+		this->x = x; this->y = y; this->val = val;
+		this->type = PLANT;
+	}
+};
+struct VerticalSegment : X {
+	int miny, maxy;
+	VerticalSegment(int x, int miny, int maxy) {
+		this->x = x; this->miny = miny; this->maxy = maxy;
+		this->type = VSEGMENT;
+	}	
+};
+// custom comparator used later on for sorting
+bool cmp(const X* a, const X* b) { return a->x < b->x; }
 
+
+// data structure used for range updates and single point queries
 struct FenwickTree {
-  vector<int> ft;
-
-  FenwickTree(int n) { ft.assign(n+1, 0); }
-
-  int rsq(int b) {
-    int sum = 0;
-    for (; b; b -= LSOne(b)) sum += ft[b];
-    return sum;
-  }
-
-  int rsq(int a, int b) {
-    return rsq(b) - (a == 1 ? 0 : rsq(a-1));
-  }
-
-  void adjust(int k, int v) {
-    for (; k < ft.size(); k += LSOne(k)) ft[k] += v;
-  }
-
-  void range_adj(int i, int j, int v) {
-    adjust(i, v);
-    adjust(j+1, -v);
-  }
+	vector<int> ft;
+	FenwickTree(int n) { ft.assign(n+1, 0); }	
+	// prefix sum query
+	int psq(int b) {
+		int sum = 0;
+		for (; b; b -= (b & -b)) sum += ft[b];
+		return sum;
+	}
+	// range sum query
+	int rsq(int a, int b) { return psq(b) - psq(a-1); }
+	// add v to k'th position (and propagate)
+	void add(int k, int v) {
+		for (; k < ft.size(); k += (k & -k)) ft[k] += v;
+	}
+	// add v to range [i..j] (and propagate)
+	void range_add(int i, int j, int v) {
+		add(i, v); add(j+1, -v);
+	}
 };
 
-
 int main() {
-  while (scanf("%d%d", &P, &V) == 2) {
+    while (scanf("%d%d", &P, &V) == 2) { // for each input case
 
-    set<int> ycoords;
-    umap<int,int> coordmap;
-    
-    rep(i,0,P-1) {
-      int x,y; scanf("%d%d",&x,&y);
-      plant_pts[i] = {x,y};
-      ycoords.insert(y);
-    }
+		set<int> ys; // use ordered set to keep track of y coordinates in increasing order
+		vector<X*> xs; // polymorphic list of X objects
 
-    rep(i,0,V-1) {
-      int x,y; scanf("%d%d",&x,&y);
-      fence_pts[i] = {x,y};
-      ycoords.insert(y);
-    }
+		// read plant coordinates
+		rep(i,1,P) {
+			int x, y; scanf("%d%d", &x, &y);
+			xs.push_back(new Plant(x, y, i));
+			ys.insert(y);
+		}
 
-    int index = 1;
-    for (int y : ycoords)
-      coordmap[y] = index++;
+		// read fence's vertices coordinates
+		vector<ii> vcoords;
+		rep(i,1,V) {
+			int x,y; scanf("%d%d", &x, &y); vcoords.emplace_back(x,y);
+			ys.insert(y);
+		}
+		rep(i,0,V-1) {
+			int j = (i+1) % V;
+			if (vcoords[i].first == vcoords[j].first) { // vertical segment
+				int miny = vcoords[i].second;
+				int maxy = vcoords[j].second;
+				if (miny > maxy) swap(miny, maxy);
+				xs.push_back(new VerticalSegment(vcoords[i].first, miny, maxy));
+			}
+		}
+		
+		// sort X objects using custom comparator
+		sort(xs.begin(), xs.end(), cmp);
 
-    vector<Segment> segments;
-    rep(i,0,P-1) {
-      Point& p = plant_pts[i];
-      int id = coordmap[p.y];
-      segments.push_back({i+1, id, id, p.x});
-    }
-    rep(i,0,V-1) {
-      int j = (i+1) % V;
-      if (fence_pts[i].x == fence_pts[j].x) {
-        int id1 = coordmap[fence_pts[i].y];
-        int id2 = coordmap[fence_pts[j].y];
-        if (id2 < id1) swap(id1, id2);
-        segments.push_back({-1, id1, id2, fence_pts[i].x });
-      }
-    }
-    sort(segments.begin(), segments.end());
+		// range compression: map all y values to a narrower range
+		int idx = 1;
+		umap<int,int> y2idx;
+		for (int y : ys) y2idx[y] = idx++;
 
-    ll sum = 0;
-    FenwickTree ft(P+V);
-    for (Segment& seg : segments) {
-      if (seg.val == -1) { // vertical segment
-        if (ft.rsq(seg.ymin) > 0) {
-          ft.range_adj(seg.ymin,seg.ymax-1,-1);
-        } else {
-          ft.range_adj(seg.ymin,seg.ymax-1, 1);
-        }
-      } else { // point
-        if (ft.rsq(seg.ymin) == 0)
-          sum += seg.val;
-      }
-    }
+		// ---------------------------------
+		// run sweep line: left to right
+		FenwickTree ft(y2idx.size());
+		ll sum = 0; // use long long (avoid int overflow)
+		for (X* p : xs) {
+			if (p->type == VSEGMENT) { // vertical segment encountered
+				VerticalSegment* vseg = static_cast<VerticalSegment*>(p);
+				int a = y2idx[vseg->miny];
+				int b = y2idx[vseg->maxy];
+				if (ft.psq(a) == 0) { // nothing in range
+					// add 1 to range -> we are now inside the fence
+					ft.range_add(a, b-1, 1);
+				} else {
+					// substract 1 to range -> we are not outside the fence
+					ft.range_add(a, b-1, -1);
+				}
+			} else { // plant encountered
+				Plant* plant = static_cast<Plant*>(p);
+				int idx = y2idx[plant->y];
+				// if outside fence -> add plant's value to sum
+				if (ft.psq(idx) == 0) sum += plant->val;
+			}
+			delete p; // clear memory
+		}
+		// print answer
+		printf("%lld\n", sum);
 
-    printf("%lld\n", sum);
-
-  }
-  return 0;
+	}
+    return 0;
 }
