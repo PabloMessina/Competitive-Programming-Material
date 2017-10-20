b5d2efe37a75da45fe80900b6c9a8f4851859aba
PabloMessina <pamessina@uc.cl>
2017-10-20 15:58:49 -0300

    update

---
+++ Solved problems/Red de Programación Competitiva/TCP-2017/D_CocinaDeProgramadores.cpp
@@ -0,0 +1,79 @@
+// tags: DP top-down, graphs, DAG
+// DISCLAIMER: only tested with example
+#include <bits/stdc++.h> // add almost everything in one shot
+using namespace std;
+// defines
+#define rep(i,a,b) for(int i = a; i <= b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+#define umap unordered_map
+#define uset unordered_set
+// typedefs
+typedef unsigned int uint;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
+typedef tuple<int,int,int> iii;
+typedef pair<double, int> pdi;
+typedef pair<ll, int> lli;
+// -------------------------------
+
+// map names to ids
+int ID = 0;
+map<string,int> name2id;
+int get_id(string& name) {
+    auto it = name2id.find(name);
+    if (it == name2id.end())
+        return name2id[name] = ID++;
+    return it->second;
+}
+
+const int MAXI = 100000;
+int times[MAXI]; 
+vector<vi> g(MAXI); // dependency graph
+
+// top down DP
+int memo[MAXI];
+int total_time(int u) {
+    if (memo[u] >= 0) return memo[u];
+    int t = 0;
+    for (int v : g[u]) {
+        t = max(t, total_time(v));
+    }
+    return t + times[u];
+}
+
+int main() {
+    int I;
+    scanf("%d", &I);
+
+    // read input and build graph
+    char buff[30];
+    rep(i,0,I-1) {
+        int t, d;
+        scanf("%d%d%s", &t, &d, buff);
+        string uname(buff);
+        int u = get_id(uname);
+        times[u] = t;
+        rep(j,1,d) {
+            scanf("%s", buff);
+            string vname(buff);
+            int v = get_id(vname);
+            g[u].push_back(v);
+        }
+    }
+
+    // find answer with top-down DP over the dependency graph
+    int N;
+    scanf("%d", &N);
+    memset(memo, -1, sizeof memo);
+    int ans = 0;
+    while (N--) {
+        scanf("%s", buff);
+        string name(buff);
+        int id = get_id(name);
+        ans = max(ans, total_time(id));
+    }
+    printf("%d\n", ans);
+    return 0;
+}
--- Solved problems/URI/KillTheWerewolf.cpp
+++ Solved problems/URI/KillTheWerewolf.cpp
@@ -9,119 +9,119 @@ int indegree[MAXN];
 int g[MAXN][2];
 
 struct Dinic {
-  struct edge {
-    int to, rev;
-    ll f, cap;
-  };
+    struct edge {
+        int to, rev;
+        ll f, cap;
+    };
 
-  vector<vector<edge>> g;
-  vector<ll> dist;
-  vector<int> q, work;
-  int n, sink;
+    vector<vector<edge>> g;
+    vector<ll> dist;
+    vector<int> q, work;
+    int n, sink;
 
-  bool bfs(int start, int finish) {
-    dist.assign(n, -1);
-    dist[start] = 0;
-    int head = 0, tail = 0;
-    q[tail++] = start;
-    while (head < tail) {
-      int u = q[head++];
-      for (const edge &e : g[u]) {
-        int v = e.to;
-        if (dist[v] == -1 and e.f < e.cap) {
-          dist[v] = dist[u] + 1;
-          q[tail++] = v;
+    bool bfs(int start, int finish) {
+        dist.assign(n, -1);
+        dist[start] = 0;
+        int head = 0, tail = 0;
+        q[tail++] = start;
+        while (head < tail) {
+            int u = q[head++];
+            for (const edge &e : g[u]) {
+                int v = e.to;
+                if (dist[v] == -1 and e.f < e.cap) {
+                    dist[v] = dist[u] + 1;
+                    q[tail++] = v;
+                }
+            }
         }
-      }
+        return dist[finish] != -1;
     }
-    return dist[finish] != -1;
-  }
 
-  ll dfs(int u, ll f) {
-    if (u == sink)
-      return f;
-    for (int &i = work[u]; i < (int)g[u].size(); ++i) {
-      edge &e = g[u][i];
-      int v = e.to;
-      if (e.cap <= e.f or dist[v] != dist[u] + 1)
-        continue;
-      ll df = dfs(v, min(f, e.cap - e.f));
-      if (df > 0) {
-        e.f += df;
-        g[v][e.rev].f -= df;
-        return df;
-      }
+    ll dfs(int u, ll f) {
+        if (u == sink)
+            return f;
+        for (int &i = work[u]; i < (int)g[u].size(); ++i) {
+            edge &e = g[u][i];
+            int v = e.to;
+            if (e.cap <= e.f or dist[v] != dist[u] + 1)
+                continue;
+            ll df = dfs(v, min(f, e.cap - e.f));
+            if (df > 0) {
+                e.f += df;
+                g[v][e.rev].f -= df;
+                return df;
+            }
+        }
+        return 0;
     }
-    return 0;
-  }
 
-  Dinic(int n) {
-    this->n = n;
-    g.resize(n);
-    dist.resize(n);
-    q.resize(n);
-  }
-
-  void add_edge(int u, int v, ll cap) {
-    edge a = {v, (int)g[v].size(), 0, cap};
-    edge b = {u, (int)g[u].size(), 0, cap};
-    g[u].push_back(a);
-    g[v].push_back(b);
-  }
-
-  ll max_flow(int source, int dest) {
-    sink = dest;
-    ll ans = 0;
-    while (bfs(source, dest)) {
-      work.assign(n, 0);
-      while (ll delta = dfs(source, LLONG_MAX))
-        ans += delta;
+    Dinic(int n) {
+        this->n = n;
+        g.resize(n);
+        dist.resize(n);
+        q.resize(n);
     }
-    return ans;
-  }
+
+    void add_edge(int u, int v, ll cap) {
+        edge a = {v, (int)g[v].size(), 0, cap};
+        edge b = {u, (int)g[u].size(), 0, cap};
+        g[u].push_back(a);
+        g[v].push_back(b);
+    }
+
+    ll max_flow(int source, int dest) {
+        sink = dest;
+        ll ans = 0;
+        while (bfs(source, dest)) {
+            work.assign(n, 0);
+            while (ll delta = dfs(source, LLONG_MAX))
+                ans += delta;
+        }
+        return ans;
+    }
 };
 
 
 int main() {
-  int n; scanf("%d", &n);
-  memset(indegree, 0, sizeof indegree);
-  rep(i,0,n-1) {
-    int a,b; scanf("%d%d",&a,&b); --a,--b;
-    g[i][0] = a, g[i][1] = b;
-    indegree[a]++;
-    indegree[b]++;
-  }
-
-  int count = 0;
-  rep(w,0,n-1) {
-    if (indegree[w] < 2) {
-      count++; continue;
-    }
-    
-    int wa = g[w][0], wb = g[w][1];
-    int s = 2*n, t = 2*n+1;
-    Dinic din(2*n+2);
-
+    int n; scanf("%d", &n);
+    memset(indegree, 0, sizeof indegree);
     rep(i,0,n-1) {
-      int a = g[i][0], b = g[i][1];
-      if (i == w or a == w or b == w) continue;
-      din.add_edge(s, i, 1);
-      din.add_edge(i, n+a, 1);
-      din.add_edge(i, n+b, 1);      
-    }
-    rep(i, 0, n-1) {
-      if (i == w) continue;
-      if (i == wa or i == wb)
-        din.add_edge(n+i, t, indegree[w]-2);
-      else
-        din.add_edge(n+i, t, indegree[w]-1);
+        int a,b; scanf("%d%d",&a,&b); --a,--b;
+        g[i][0] = a, g[i][1] = b;
+        indegree[a]++;
+        indegree[b]++;
     }
 
-    int votes = n - indegree[w] - 1;
-    int mf = (int)din.max_flow(s, t);
-    if (votes > mf)
-      count++;
-  }
-  printf("%d\n", count);
-  return 0;
+    int count = 0;
+    rep(w,0,n-1) {
+        if (indegree[w] < 2) {
+            count++; continue;
+        }
+        
+        int wa = g[w][0], wb = g[w][1];
+        int s = 2*n, t = 2*n+1;
+        Dinic din(2*n+2);
+
+        rep(i,0,n-1) {
+            int a = g[i][0], b = g[i][1];
+            if (i == w or a == w or b == w) continue;
+            din.add_edge(s, i, 1);
+            din.add_edge(i, n+a, 1);
+            din.add_edge(i, n+b, 1);      
+        }
+        rep(i, 0, n-1) {
+            if (i == w) continue;
+            if (i == wa or i == wb)
+                din.add_edge(n+i, t, indegree[w]-2);
+            else
+                din.add_edge(n+i, t, indegree[w]-1);
+        }
+
+        int votes = n - indegree[w] - 1;
+        int mf = (int)din.max_flow(s, t);
+        if (votes > mf)
+            count++;
+    }
+    printf("%d\n", count);
+    return 0;
 }
