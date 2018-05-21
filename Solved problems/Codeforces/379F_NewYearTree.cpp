61d2feb8baa21c72d1a85c137e7c542c9e0086a9
PabloMessina <pamessina@uc.cl>
2018-05-21 01:42:49 -0400

    update

--- Solved problems/Codeforces/379F_NewYearTree.cpp
+++ Solved problems/Codeforces/379F_NewYearTree.cpp
@@ -1,98 +1,90 @@
-// tags: LCA, level ancestor, jump pointers, sparse table
-#include <bits/stdc++.h>
+// tags: LCA, LA (level acenstor), jump pointers, binary lifting, tree diameter, sparse tables
+#include <bits/stdc++.h> // add almost everything in one shot
 using namespace std;
+// defines
+#define rep(i,a,b) for(int i = a; i <= b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+#define umap unordered_map
+#define uset unordered_set
+// typedefs
+typedef unsigned int uint;
+typedef unsigned long long int ull;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef tuple<int,int,int> iii;
+// -------------------------------
 
-#define MAXN 1000010
-#define MAXLOG 20
-
-#define rep(i,a,b) for(int i=a; i<=b; ++i)
-#define invrep(i,b,a) for(int i=b; i>=a; --i)
-
-int P[MAXN][MAXLOG+1];
-int L[MAXN];
-int e1, e2; // diameter's ends
-int diam; // diameter's length
-int n;
-
-void init() {
-    memset(P, -1, sizeof P);
-    L[0] = 0;
-    rep (i,1,3) P[i][0] = 0, L[i] = 1;
-    e1 = 1;
-    e2 = 2;
-    diam = 2;
-    n = 4;
-}
-
-int log2(int x) {
-    int i = -1;
-    while (x) x >>= 1, ++i;
-    return i;
-}
-
-int lev_anc(int u, int k) {
-    if (k == 0) return u;
-    invrep(j, log2(k), 0) {
-        if (k >= (1 << j)) {
-            u = P[u][j];
-            k -= (1 << j);
-            if (k == 0) break;
+namespace LCA {
+    const int MAXN = 5 * 100000 * 2 + 4;
+    const int MAXLOG = 31 - __builtin_clz(MAXN);
+    int P[MAXN][MAXLOG+1];
+    int D[MAXN];
+    int n = 4;
+    void init() {
+        memset(P, -1, sizeof P);
+        D[0] = 0;
+        rep(u,1,3) {
+            P[u][0] = 0;
+            D[u] = 1;
         }
     }
-    return u;
-}
-
-int LCA(int u, int v) {
-    if (L[u] < L[v]) swap(u,v);
-    int diff = L[u] - L[v];
-    u = lev_anc(u, diff);
-    if (u == v) return u;
-    invrep(j, MAXLOG, 0)
-        if ((P[u][j] != -1) and (P[u][j] != P[v][j]))
-            u = P[u][j], v = P[v][j];
-    return P[u][0];
-}
-
-int dist(int u, int v) {
-    return L[u] + L[v] - 2 * L[LCA(u,v)];
-}
-
-int add_child(int u, int v) {
-    // update level
-    L[v] = L[u] + 1;
-    // update ancestors
-    P[v][0] = u;
-    rep (j, 1, MAXLOG){
-        P[v][j] = P[P[v][j-1]][j-1];
-        if (P[v][j] == -1) break;
+    void add_child(int p) {
+        P[n][0] = p;
+        D[n] = D[p] + 1;
+        rep(i,1,MAXLOG) {
+            p = P[p][i-1];
+            if (p == -1) break;
+            P[n][i] = p;
+        }
+        n++;
+    }
+    int raise(int u, int dist) {
+        for (int i = 0; dist; i++, dist>>=1) if (dist&1) u = P[u][i];
+        return u;
+    }
+    int get_lca(int u, int v) {        
+        if (D[u] < D[v]) swap(u, v);        
+        int diff = D[u] - D[v];
+        u = raise(u, diff);
+        if (u == v) return u;
+        invrep(i, MAXLOG, 0) {
+            if (P[u][i] != P[v][i]) {
+                u = P[u][i];
+                v = P[v][i];
+            }
+        }
+        return P[u][0];
+    }
+    int dist(int u, int v) {   
+        return D[u] + D[v] - 2 * D[get_lca(u,v)];
     }
 }
 
-int main () {
-    init();
-
-    int q; scanf("%d",&q);  
-    while (q--) {
-        int v; scanf("%d", &v); --v;
-        // update diamater
-        int d1 = dist(v, e1);
-        int d2 = dist(v, e2);
-        int maxd, f;
-        if (d1 > d2)
-            maxd = d1, f = e1;
-        else
-            maxd = d2, f = e2;
-        if (maxd == diam) {
-            diam++;
-            e1 = f;
-            e2 = n;
+int main() {
+    int q; scanf("%d", &q);
+    int diam = 2;
+    int e1 = 1, e2 = 2;
+    LCA::init();
+    while(q--) {
+        int u; scanf("%d", &u); u--;
+        int far_e, far_dist;
+        int d1 = LCA::dist(u, e1);
+        int d2 = LCA::dist(u, e2);
+        if (d1 > d2) {
+            far_e = e1;
+            far_dist = d1;            
+        } else {
+            far_e = e2;
+            far_dist = d2;
         }
-        // add 2 children
-        add_child(v, n);
-        add_child(v, n+1);
-        // update n
-        n += 2;
-        // answer
+        if (far_dist == diam) {
+            diam = far_dist + 1;
+            e1 = far_e;
+            e2 = LCA::n;            
+        }
+        LCA::add_child(u);
+        LCA::add_child(u);
         printf("%d\n", diam);
     }
     return 0;
