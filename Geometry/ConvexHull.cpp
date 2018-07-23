00111673a883d10b3798e5f5cdfd382639439f34
PabloMessina <pamessina@uc.cl>
2018-07-23 02:43:45 -0400

    update

--- Geometry/ConvexHull.cpp
+++ Geometry/ConvexHull.cpp
@@ -1,67 +1,77 @@
-// Andrew's Montone Chain Algorithm
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i = a; i <= b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+typedef long long int ll;
+// ----------------------------------------------
+// Convex Hull: Andrew's Montone Chain Algorithm 
+// ----------------------------------------------
+
 struct Point {
-  double x, y;
-  bool operator<(const Point& p) {
-    return x < p.x || (x == p.x && y < p.y);
-  }
+    ll x, y;
+    bool operator<(const Point& p) {
+        return x < p.x || (x == p.x && y < p.y);
+    }
 };
 
-double isLeft(Point o, Point a, Point b) {
-  return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
+ll cross(Point& a, Point& b, Point& c) {
+    ll dx0 = b.x - a.x, dy0 = b.y - a.y;
+    ll dx1 = c.x - a.x, dy1 = c.y - a.y;
+    return dx0 * dy1 - dx1 * dy0;
 }
 
 vector<Point> upper_hull(vector<Point>& P) {
-  // sort points lexicographically
-  int n = P.size(), k = 0;
-  sort(P.begin(), P.end());
+    // sort points lexicographically
+    int n = P.size(), k = 0;
+    sort(P.begin(), P.end());
 
-  // build upper hull
-  vector<Point> uh(n);
-  invrep (i, n-1, 0) {
-    while (k >= 2 && isLeft(uh[k-2], uh[k-1], P[i]) <= 0) k--;
-    uh[k++] = P[i];
-  }
-  uh.resize(k);
-  return uh;
+    // build upper hull
+    vector<Point> uh(n);
+    invrep (i, n-1, 0) {
+        while (k >= 2 && cross(uh[k-2], uh[k-1], P[i]) <= 0) k--;
+        uh[k++] = P[i];
+    }
+    uh.resize(k);
+    return uh;
 }
 
 vector<Point> lower_hull(vector<Point>& P) {
-  // sort points lexicographically
-  int n = P.size(), k = 0;
-  sort(P.begin(), P.end());
+    // sort points lexicographically
+    int n = P.size(), k = 0;
+    sort(P.begin(), P.end());
 
-  // collect lower hull
-  vector<Point> lh(n);
-  rep (i, 0, n-1) {
-    while (k >= 2 && isLeft(lh[k-2], lh[k-1], P[i]) <= 0) k--;
-    lh[k++] = P[i];
-  }
-  lh.resize(k);
-  return lh;
+    // collect lower hull
+    vector<Point> lh(n);
+    rep (i, 0, n-1) {
+        while (k >= 2 && cross(lh[k-2], lh[k-1], P[i]) <= 0) k--;
+        lh[k++] = P[i];
+    }
+    lh.resize(k);
+    return lh;
 }
 
 vector<Point> convex_hull(vector<Point>& P) {
-  int n = P.size(), k = 0;
+    int n = P.size(), k = 0;
 
-  // set initial capacity
-  vector<Point> H(2*n);
+    // set initial capacity
+    vector<Point> H(2*n);
 
-  // Sort points lexicographically
-  sort(P.begin(), P.end());
+    // Sort points lexicographically
+    sort(P.begin(), P.end());
 
-  // Build lower hull
-  for (int i = 0; i < n; ++i) {
-    while (k >= 2 && isLeft(H[k-2], H[k-1], P[i]) <= 0) k--;
-    H[k++] = P[i];
-  }
+    // Build lower hull
+    for (int i = 0; i < n; ++i) {
+        while (k >= 2 && cross(H[k-2], H[k-1], P[i]) <= 0) k--;
+        H[k++] = P[i];
+    }
 
-  // Build upper hull
-  for (int i = n-2, t = k+1; i >= 0; i--) {
-    while (k >= t && isLeft(H[k-2], H[k-1], P[i]) <= 0) k--;
-    H[k++] = P[i];
-  }
+    // Build upper hull
+    for (int i = n-2, t = k+1; i >= 0; i--) {
+        while (k >= t && cross(H[k-2], H[k-1], P[i]) <= 0) k--;
+        H[k++] = P[i];
+    }
 
-  // remove extra space
-  H.resize(k-1);
-  return H;
+    // remove extra space
+    H.resize(k-1);
+    return H;
 }
