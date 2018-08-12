403a44188da86acbd29be30b7232aac58209c32f
PabloMessina <pamessina@uc.cl>
2018-08-12 15:37:38 -0300

    update

---
+++ Solved problems/SPOJ/CROSSPDCT_Left-right-or-center.cpp
@@ -0,0 +1,25 @@
+// tags: geometry, cross product
+#include <bits/stdc++.h> // import everything in one shot
+using namespace std;
+typedef long long int ll;
+// -------------------------------
+// references:
+// https://github.com/PabloMessina/Competitive-Programming-Material/blob/master/Geometry/Geometry2DUtils.cpp
+// https://www.geeksforgeeks.org/orientation-3-ordered-points/
+struct Point { ll x, y; };
+ll cross(Point& a, Point& b, Point& c) {
+    ll dx0 = b.x - a.x, dy0 = b.y - a.y;
+    ll dx1 = c.x - a.x, dy1 = c.y - a.y;
+    return dx0 * dy1 - dy0 * dx1;
+}
+int main() {
+    Point a, b; int n;
+    cin >> a.x >> a.y >> b.x >> b.y >> n;
+    while (n--) {
+        Point p; cin >> p.x >> p.y;
+        ll tmp = cross(a, b, p);
+        char ans = tmp > 0 ? 'I' : tmp < 0 ? 'D' : 'C';
+        cout << ans << endl;
+    }
+    return 0;
+}
--- Solved problems/URI/DatingOn-Line.cpp
+++ Solved problems/URI/DatingOn-Line.cpp
@@ -1,4 +1,4 @@
-// tags: greedy, polygon area
+// tags: greedy, polygon area, geometry
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,a,b) for(int i=a;i<=b;++i)
