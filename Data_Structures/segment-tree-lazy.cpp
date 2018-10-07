a24ec26dcbe4cd24ac92603e21a27d89a4ac02ab
PabloMessina <pamessina@uc.cl>
2018-10-07 12:18:39 -0300

    update

--- Data_Structures/segment-tree-lazy.cpp
+++ Data_Structures/segment-tree-lazy.cpp
@@ -3,87 +3,87 @@ using namespace std;
 typedef vector<int> vi;
 
 struct SegmentTreeLazy {
-  vi arr, tree, lazy;
-  int n;
-  inline int left (int p) { return p << 1; }
-  inline int right(int p) { return (p << 1) + 1; }
-   
-  // build the tree
-  void build(int node, int a, int b) {
-    if(a > b) return; // out of range 
-    if(a == b) { // leaf node
-      tree[node] = arr[a]; // init value
-      return;
+    vi arr, tree, lazy;
+    int n;
+    inline int left (int p) { return p << 1; }
+    inline int right(int p) { return (p << 1) + 1; }
+     
+    // build the tree
+    void build(int node, int a, int b) {
+        if(a > b) return; // out of range 
+        if(a == b) { // leaf node
+            tree[node] = arr[a]; // init value
+            return;
+        }
+        int lnode = left(node), rnode = right(node);
+        build(lnode, a, (a+b)/2); // init left child
+        build(rnode, (a+b)/2 + 1, b); // init right child 
+        tree[node] = max(tree[lnode], tree[rnode]); // init root value
     }
-    int lnode = left(node), rnode = right(node);
-    build(lnode, a, (a+b)/2); // init left child
-    build(rnode, (a+b)/2 + 1, b); // init right child 
-    tree[node] = max(tree[lnode], tree[rnode]); // init root value
-  }
-   
-  // increment elements within range [i, j] with value
-  void range_update(int node, int a, int b, int i, int j, int value) {
-    if(lazy[node] != 0) { // this node needs to be updated
-      tree[node] += lazy[node]; // update it 
-      if(a != b) {
-        lazy[left(node)] += lazy[node]; // mark left child as lazy
-        lazy[right(node)] += lazy[node]; // mark right child as lazy  
-      }   
-      lazy[node] = 0; // Reset it
+     
+    // increment elements within range [i, j] with value
+    void range_update(int node, int a, int b, int i, int j, int value) {
+        if(lazy[node] != 0) { // this node needs to be updated
+            tree[node] += lazy[node]; // update it 
+            if(a != b) {
+                lazy[left(node)] += lazy[node]; // mark left child as lazy
+                lazy[right(node)] += lazy[node]; // mark right child as lazy  
+            }   
+            lazy[node] = 0; // Reset it
+        }
+        
+        if(a > b || a > j || b < i) // current segment is not within range [i, j]
+            return;
+            
+        if(a >= i && b <= j) { // segment is fully within range
+            tree[node] += value; 
+            if(a != b) { // not leaf node
+                lazy[left(node)] += value;
+                lazy[right(node)] += value;
+            } 
+            return;
+        }
+     
+        range_update(left(node), a, (a+b)/2, i, j, value); // updating left child
+        range_update(right(node), 1+(a+b)/2, b, i, j, value); // updating right child 
+        tree[node] = max(tree[left(node)], tree[right(node)]); // Updating root with max value
     }
-    
-    if(a > b || a > j || b < i) // current segment is not within range [i, j]
-      return;
-      
-    if(a >= i && b <= j) { // segment is fully within range
-      tree[node] += value; 
-      if(a != b) { // not leaf node
-        lazy[left(node)] += value;
-        lazy[right(node)] += value;
-      } 
-      return;
+     
+    // query tree to get max element value within range [i, j]
+    int range_query(int node, int a, int b, int i, int j) {  
+        if(a > b || a > j || b < i) return INT_MIN; // out of range 
+        if(lazy[node] != 0) { // this node needs to be updated
+            tree[node] += lazy[node]; // update it 
+            if(a != b) {
+                lazy[left(node)] += lazy[node]; // mark child as lazy
+                lazy[right(node)] += lazy[node]; // mark child as lazy
+            } 
+            lazy[node] = 0; // reset it
+        } 
+        if(a >= i && b <= j) // current segment is totally within range [i, j]
+            return tree[node]; 
+        int q1 = range_query(left(node), a, (a+b)/2, i, j); // Query left child
+        int q2 = range_query(right(node), 1+(a+b)/2, b, i, j); // Query right child 
+        return = max(q1, q2); // Return final result  
     }
-   
-    range_update(left(node), a, (a+b)/2, i, j, value); // updating left child
-    range_update(right(node), 1+(a+b)/2, b, i, j, value); // updating right child 
-    tree[node] = max(tree[left(node)], tree[right(node)]); // Updating root with max value
-  }
-   
-  // query tree to get max element value within range [i, j]
-  int range_query(int node, int a, int b, int i, int j) {  
-    if(a > b || a > j || b < i) return INT_MIN; // out of range 
-    if(lazy[node] != 0) { // this node needs to be updated
-      tree[node] += lazy[node]; // update it 
-      if(a != b) {
-        lazy[left(node)] += lazy[node]; // mark child as lazy
-        lazy[right(node)] += lazy[node]; // mark child as lazy
-      } 
-      lazy[node] = 0; // reset it
-    } 
-    if(a >= i && b <= j) // current segment is totally within range [i, j]
-      return tree[node]; 
-    int q1 = range_query(left(node), a, (a+b)/2, i, j); // Query left child
-    int q2 = range_query(right(node), 1+(a+b)/2, b, i, j); // Query right child 
-    return = max(q1, q2); // Return final result  
-  }
 
-  SegmentTree(const vi& A) {
-    arr = A; n = (int)A.size();              // copy content for local usage
-    tree.assign(4 * n, 0);            // create large enough vector of zeroes
-    lazy.assign(4 * n, 0);
-    build(1, 0, n - 1);                                  // recursive build
-  }
-  // overloading
-  int range_update(int i, int j, int value) { return range_update(1, 0, n - 1, i, j, value); }
-  int range_query(int i, int j) { return range_query(1, 0, n - 1, i, j); }
+    SegmentTree(const vi& A) {
+        arr = A; n = (int)A.size();              // copy content for local usage
+        tree.assign(4 * n, 0);            // create large enough vector of zeroes
+        lazy.assign(4 * n, 0);
+        build(1, 0, n - 1);                                  // recursive build
+    }
+    // overloading
+    int range_update(int i, int j, int value) { return range_update(1, 0, n - 1, i, j, value); }
+    int range_query(int i, int j) { return range_query(1, 0, n - 1, i, j); }
 
 }; 
 
 // usage
 int main() {
-  vi A = { 18, 17, 13, 19, 15, 11, 20 };
-  SegmentTreeLazy stl(A);
-  stl.range_update(1, 5, 100);
-  stl.range_query(1, 3);
-  return 0;
+    vi A = { 18, 17, 13, 19, 15, 11, 20 };
+    SegmentTreeLazy stl(A);
+    stl.range_update(1, 5, 100);
+    stl.range_query(1, 3);
+    return 0;
 }
---
+++ Solved problems/SPOJ/VBOSS_WhoIsTheBoss.cpp
@@ -0,0 +1,73 @@
+// tags: sorting, ordered sets (std::set), top-down DP,
+// graphs, trees
+#include <bits/stdc++.h> // import everything in one shot
+using namespace std;
+#define rep(i,a,b) for(int i = a; i <= b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+#define umap unordered_map
+// -------------------------------
+
+const int MAXM = 30000;
+
+struct Employee {
+    int ID, salary, height;
+    bool operator<(const Employee& o) const {
+        if (height == o.height) return salary < o.salary;
+        return height < o.height;
+    }
+} employees[MAXM];
+int parent[MAXM];
+vector<int> children[MAXM];
+
+int memo[MAXM];
+int count_descendants(int i) { // top-down dp
+    if (memo[i] != -1) return memo[i];
+    int count = children[i].size();
+    for (int j : children[i]) count += count_descendants(j);
+    return memo[i] = count;
+}
+int get_parent(int i) {
+    if (parent[i] == -1) return 0;
+    return employees[parent[i]].ID;
+}
+
+bool salary_cmp(int i, int j) {
+    return employees[i].salary < employees[j].salary;
+}
+
+int main() {
+    // read input
+    int n; cin >> n;
+    while (n--) {
+        int m, q; cin >> m >> q;        
+        rep(i,0,m-1) {
+            Employee& e = employees[i];
+            cin >> e.ID >> e.salary >> e.height;
+            children[i].clear();
+        }
+        // sort employees lexicographically
+        sort(employees, employees + m);
+        // map IDs to indexes
+        umap<int,int> id2index;
+        rep(i,0,m-1) id2index[employees[i].ID] = i;
+        // find immediate boss for each employee
+        set<int, bool(*)(int,int)> active(salary_cmp);
+        active.insert(m-1);
+        parent[m-1] = -1;
+        invrep(i, m-2, 0) {
+            auto ret = active.insert(i);
+            int b = *std::next(ret.first);
+            parent[i] = b;
+            children[b].push_back(i);
+        }
+        // answer queries
+        memset(memo, -1, sizeof(int) * m);
+        while (q--) {
+            int id; cin >> id;
+            int i = id2index[id];
+            cout << get_parent(i) << " "
+                 << count_descendants(i) << endl;
+        }
+    }
+    return 0;
+}
