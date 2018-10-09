2ec9e2e4c5c9c43ff1d57625c4ce6c0bf8e25ca7
PabloMessina <pamessina@uc.cl>
2018-10-09 17:01:37 -0300

    update

--- Solved problems/URI/GameOfMatchings.cpp
+++ Solved problems/URI/GameOfMatchings.cpp
@@ -11,68 +11,60 @@ int lps[MAXN];
 int n, m;
 
 int main() {
-
-    // --- build target ---
     string input;
-    getline(cin, input);
-    m = input.size();
-
-    memset(lastIndex, -1, sizeof(lastIndex));
-    rep(i,0,m-1) {
-        int c = input[i] - 'a';
-        target[i] = (lastIndex[c] == -1 ? 0 : i - lastIndex[c]);
-        lastIndex[c] = i;
-    }
-
-    // --- build pattern ---
-    scanf("%d", &n);
-
-    memset(lastIndex, -1, sizeof(lastIndex));
-    rep(i,0,n-1) {
-        int k; scanf("%d", &k); --k;
-        pattern[i] = (lastIndex[k] == -1 ? 0 : i - lastIndex[k]);
-        lastIndex[k] = i;
-    }
-
-    // ---- KMP modified ----
-
-    int i, j, c;
-
-    // 1.- build longest proper prefix-suffix array    
-    i = 0, j = 1;
-    while (j < n) {
-        while (true) {
-            c = (i - pattern[j] >= 0 ? pattern[j] : 0);
-            if (i == 0 or pattern[i] == c) break;
-            i = lps[i-1];
-        }        
-        if (pattern[i] == c)
-            lps[j] = ++i;
-        else
-            lps[j] = 0;
-        j++;
-    }
-
-    // 2.- count pattern matches
-    i = j = 0;
-    int matches = 0;
-    while (j < m) {
-        while (true) {
-            c = (i - target[j] >= 0 ? target[j] : 0);
-            if (i == 0 or pattern[i] == c) break;
-            i = lps[i-1];
+    while (cin >> input) {
+        // --- build target ---        
+        m = input.size();
+        memset(lastIndex, -1, sizeof(lastIndex));
+        rep(i,0,m-1) {
+            int c = input[i] - 'a';
+            target[i] = (lastIndex[c] == -1 ? 0 : i - lastIndex[c]);
+            lastIndex[c] = i;
         }
-        if (pattern[i] == c) {
-            i++;
-            if (i == n) {
-                matches++;
+        // --- build pattern ---
+        cin >> n;
+        memset(lastIndex, -1, sizeof(lastIndex));
+        rep(i,0,n-1) {
+            int k; cin >> k; --k;
+            pattern[i] = (lastIndex[k] == -1 ? 0 : i - lastIndex[k]);
+            lastIndex[k] = i;
+        }
+        // ---- KMP modified ----
+        int i, j, c;
+        // 1.- build longest proper prefix-suffix array    
+        i = 0, j = 1;
+        while (j < n) {
+            while (true) {
+                c = (i - pattern[j] >= 0 ? pattern[j] : 0);
+                if (i == 0 or pattern[i] == c) break;
+                i = lps[i-1];
+            }        
+            if (pattern[i] == c)
+                lps[j] = ++i;
+            else
+                lps[j] = 0;
+            j++;
+        }
+        // 2.- count pattern matches
+        i = j = 0;
+        int matches = 0;
+        while (j < m) {
+            while (true) {
+                c = (i - target[j] >= 0 ? target[j] : 0);
+                if (i == 0 or pattern[i] == c) break;
                 i = lps[i-1];
             }
+            if (pattern[i] == c) {
+                i++;
+                if (i == n) {
+                    matches++;
+                    i = lps[i-1];
+                }
+            }
+            j++;
         }
-        j++;
+        // --- print output ---
+        cout << matches << endl;
     }
-
-    // --- print output ---
-    printf("%d\n", matches);
     return 0;
 }
