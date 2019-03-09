e5b1461116211606f0444f9216833093ecc4eac6
PabloMessina <pamessina@uc.cl>
2019-03-09 11:53:55 -0300

    update

--- Solved problems/kattis/ReversedBinaryNumbers.cpp
+++ Solved problems/kattis/ReversedBinaryNumbers.cpp
@@ -1,13 +1,11 @@
 // tags: bitwise operators, bit manipulation, implementation
 #include <bits/stdc++.h>
 using namespace std;
-
 int reverse_bits(int x) {
     int v = 0;
     while (x) v <<= 1, v |= x&1, x >>= 1;
     return v;
 }
-
 int main() {
     int n; cin >> n; cout << reverse_bits(n);
     return 0;
