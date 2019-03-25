1c6e3cae0afc9120d876a72bc5b1955b4501cccf
PabloMessina <pamessina@uc.cl>
2019-03-24 23:05:19 -0300

    update

--- Search/BinarySearch.cpp
+++ Search/BinarySearch.cpp
@@ -43,31 +43,22 @@ int upperbound(vector<int> a, int key, int i, int j) {
     return i
 }
 
-/* ============================= */
-/*  upper_bound(), lower_bound() */
-/* ============================= */
+/* ======================================= */
+/*  std::upper_bound(), std::lower_bound() */
+/* ======================================= */
 
 // search between [first, last)
 // if no value is >= key (lb) / > key (ub), return last
 
-#include <algorithm>
-#include <iostream>     // std::cout
-#include <algorithm>    // std::lower_bound, std::upper_bound, std::sort
-#include <vector>       // std::vector
+#include <bits/stdc++.h>
 
 int main () {
-    int myints[] = {10,20,30,30,20,10,10,20};
-    std::vector<int> v(myints,myints+8);           // 10 20 30 30 20 10 10 20
-
-    std::sort (v.begin(), v.end());                // 10 10 10 20 20 20 30 30
-
-    std::vector<int>::iterator low,up;
-    low=std::lower_bound (v.begin(), v.end(), 20); //          ^
-    up= std::upper_bound (v.begin(), v.end(), 20); //                   ^
-
-    std::cout << "lower_bound at position " << (low- v.begin()) << '\n';
-    std::cout << "upper_bound at position " << (up - v.begin()) << '\n';
-
+    vector<int> v{10,20,30,30,20,10,10,20};          // 10 20 30 30 20 10 10 20
+    sort (v.begin(), v.end());                       // 10 10 10 20 20 20 30 30
+    auto low = lower_bound (v.begin(), v.end(), 20); //          ^
+    auto up  = upper_bound (v.begin(), v.end(), 20); //                 ^
+    cout << "lower_bound at position " << (low- v.begin()) << '\n';
+    cout << "upper_bound at position " << (up - v.begin()) << '\n';
     return 0;
 }
 
@@ -81,12 +72,11 @@ lower_bound(v.begin(), v.end(), x) - v.begin();
 
 v.end() - upper_bound(v.begin(), v.end(), x);
 
-//=================
-// binary_search()
-//=================
+//======================
+// std::binary_search()
+//======================
 bool myfunction (int i,int j) { return (i<j); }
-int myints[] = {1,2,3,4,5,4,3,2,1};
-std::vector<int> v(myints,myints+9);  
+std::vector<int> v{1,2,3,4,5,4,3,2,1};  
 bool found = std::binary_search (v.begin(), v.end(), 6, myfunction)
 
 
