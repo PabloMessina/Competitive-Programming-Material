e14bc128c0271f788e65f2a92d7ca79e7829a2b8
PabloMessina <pamessina@uc.cl>
2019-04-06 01:22:25 -0300

    update

--- Solved problems/kattis/NeedForSpeed.cpp
+++ Solved problems/kattis/NeedForSpeed.cpp
@@ -1,35 +1,21 @@
-// tags: galloping search, binary search
+// tags: binary search
 #include <bits/stdc++.h> // import everything in one shot
 using namespace std;
 #define rep(i,a,b) for(int i = a; i <= b; ++i)
-
-const double eps = 1e-8; // for numerical stability
-int N;
-double T;
+int N; double T;
 double dist[1000], speed[1000];
-
 double total_time(double c) {
     double t = 0;
     rep(i,0,N-1) t += dist[i] / (c + speed[i]);
     return t;
 }
-
 int main() {
     // read input
     cin >> N >> T;
     rep(i,0,N-1) cin >> dist[i] >> speed[i];
-
-    // find initial range [l,r] using galloping search
-    double offset = -*min_element(speed, speed+N) + eps;
-    double span = 1.;
-    double l = offset, r;
-    while (total_time(offset + span) > T) {
-        l = offset + span;
-        span *= 2.;
-    }
-    r = offset + span;
-
     // find best answer in range [l,r] using binary search
+    double l = -*min_element(speed, speed+N);
+    double r = 1001000.0;
     rep(_,1,100) {
         double m = (l+r)*0.5;
         if (total_time(m) > T) l = m;
