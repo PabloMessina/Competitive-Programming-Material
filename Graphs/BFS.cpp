f0aab875ccc038a459e4b437bdd2b099d06b9ccf
PabloMessina <pamessina@uc.cl>
2019-05-02 17:45:49 -0400

    update

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -1,16 +1,16 @@
-#define MAXN 1000
-vector<vi> g; // graph
-vi depth; // bfs depth per node
-int n; // num of nodes
+const int MAXN = 1000;
+vector<int> g[MAXN]; // graph
+int depth[MAXN]; // bfs depth per node
+int n; // number of nodes
 
 void bfs(int s) {
-	queue<int> q; q.push(s);
-	depth.assign(n,-1);
-	depth[s] = 0;
-	while (!q.empty()) {
-		int u = q.front(); q.pop();
-		for (int v : g[u]) {
-			if (depth[v] == -1) {
+	memset(depth, -1, sizeof(int) * n); // init depth with -1
+	queue<int> q; q.push(s); // init queue and add 's' (starting node)
+	depth[s] = 0; // s will have depth 0
+	while (!q.empty()) { // while there are nodes in the queue
+		int u = q.front(); q.pop(); // extract the first node 'u' from the queue
+		for (int v : g[u]) { // for each neighbor 'v' of 'u'
+			if (depth[v] == -1) { // if 'v' has not been visited yet -> visit it
 				depth[v] = depth[u] + 1;
 				q.push(v);
 			}
--- Graphs/DFS.cpp
+++ Graphs/DFS.cpp
@@ -1,42 +1,60 @@
 // =========================
 // Depth First Search (DFS)
 // =========================
-#define MAXN 1000
-vector<vi> g[MAXN];
+const int MAXN = 1000;
+vector<int> g[MAXN];
 bool visited[MAXN];
 int n;
 
-//iterative
-void dfs(int root) {
-	stack<int> s;
-	s.push(root);
-  visited[root] = true;
-	while (!s.empty()) {
-		int u = s.top(); s.pop();
-		for (int v : g[u])
-			if (!visited[v])
-        visited[u] = true, s.push(v);
-	}
-}
-
 //recursive
 void dfs(int u) {
-	visited[u] = true;
-	for(int v : g[u])
-		if(!visited[v]) 
-			dfs(v);
+    visited[u] = true;
+    for(int v : g[u]) {
+        if(!visited[v]) {
+            dfs(v);
+        }
+    }
+}
+
+//recursive, using depth
+int depth[MAXN];
+void dfs(int u, int d) {
+    depth[u] = d;
+    for(int v : g[u]) {
+        if(depth[v] == -1) { // not visited yet
+            dfs(v, d+1);
+        }
+    }
+}
+
+//iterative
+void dfs(int root) {
+    stack<int> s;
+    s.push(root);
+    visited[root] = true;
+    while (!s.empty()) {
+        int u = s.top(); s.pop();
+        for (int v : g[u]) {
+            if (!visited[v]) {
+                visited[u] = true;
+                s.push(v);
+            }
+        }
+    }
 }
 
 //-----------------------------
 // Finding connected components
 //-----------------------------
 int count_cc() {
-  int count = 0;
-  memset(visited,0,sizeof(bool)*n);
-  rep(i,0,n-1)
-    if (!visited[i])
-      count++, dfs(i);
-  return count;
+    int count = 0;
+    memset(visited, 0, sizeof(bool)*n);
+    rep(i,0,n-1) {
+        if (!visited[i]) {
+            count++, dfs(i);
+        }
+    }
+    return count;
 }
 
 //------------------------------
@@ -44,23 +62,25 @@ int count_cc() {
 //------------------------------
 
 //explicit graph
-#define DFS_WHITE (-1)
+const int DFS_WHITE = -1;
 vector<int> dfs_num(DFS_WHITE,n);
 void floodfill(int u, int color) {
-  dfs_num[u] = color;
-  for (int v : g[u])
-    if (dfs_num[v] == DFS_WHITE)
-      floodfill(v, color);
+    dfs_num[u] = color;
+    for (int v : g[u]) {
+        if (dfs_num[v] == DFS_WHITE) {
+            floodfill(v, color);
+        }
+    }
 }
 
 //implicit graph
 int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 const char EMPTY = '*';
 int floodfill(int r, int c, char color) {
-	if (r < 0 || r >= R || c < 0 || c >= C) return 0; // outside grid
-	if (grid[r][c] != EMPTY) return 0; // cannot be colored
-  grid[r][c] = color;
-	int ans = 1;
-	rep(i,0,3) ans += floodfill(r + dirs[i][0], c + dirs[i][1], color);
-	return ans;
+    if (r < 0 || r >= R || c < 0 || c >= C) return 0; // outside grid
+    if (grid[r][c] != EMPTY) return 0; // cannot be colored
+    grid[r][c] = color;
+    int ans = 1;
+    rep(i,0,3) ans += floodfill(r + dirs[i][0], c + dirs[i][1], color);
+    return ans;
 }
