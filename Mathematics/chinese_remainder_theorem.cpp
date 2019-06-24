3681e4eb9da2d79f8e5c6f7afe83e757f8c37fdd
PabloMessina <pamessina@uc.cl>
2019-06-23 23:08:43 -0400

    update

--- Mathematics/chinese_remainder_theorem.cpp
+++ Mathematics/chinese_remainder_theorem.cpp
@@ -22,38 +22,67 @@ void xgcd(ll a, ll b, ll& g, ll& x, ll& y) {
         r1 = r0, x1 = x0, y1 = y0;        
     }
     g = r2, x = x2, y = y2;
-    // for debugging
+    // for debugging (in case you think you might have bugs)
     // assert (g == a * x + b * y);
     // assert (g == __gcd(a,b));
 }
 
+// ==============================================
 // CRT for a system of 2 modular linear equations
+// ==============================================
+// We want to find X such that:
 //   1) x = r1 (mod m1)
 //   2) x = r2 (mod m2)
-// sol = r1 + m1 * (r2-r1)/g * x' (mod LCM(m1,m2))
+// The solution is given by:
+//    sol = r1 + m1 * (r2-r1)/g * x' (mod LCM(m1,m2))
 // where x' comes from
 //   m1 * x' + m2 * y' = g = GCD(m1,m2)
 //   where x' and y' are the values found by extended euclidean algorithm (xgcd)
+// Useful references:
+//   https://codeforces.com/blog/entry/61290
+//   https://forthright48.com/chinese-remainder-theorem-part-1-coprime-moduli
+//   https://forthright48.com/chinese-remainder-theorem-part-2-non-coprime-moduli
+// ** Note: this solution works if lcm(m1,m2) fits in a long long (64 bits)
 pair<ll,ll> CRT(ll r1, ll m1, ll r2, ll m2) {
     ll g, x, y; xgcd(m1, m2, g, x, y);
     if ((r1 - r2) % g != 0) return {-1, -1}; // no solution
     ll z = m2/g;
     ll lcm = m1 * z;
     ll sol = add(mod(r1, lcm), m1*mult(mod(x,z),mod((r2-r1)/g,z),z), lcm);
-    // for debugging
+    // for debugging (in case you think you might have bugs)
     // assert (0 <= sol and sol < lcm);
     // assert (sol % m1 == r1 % m1);
     // assert (sol % m2 == r2 % m2);
-    return {sol, lcm};
+    return {sol, lcm}; // solution + lcm(m1,m2)
 }
 
-int main() {
-    while (1) {
-        ll r1, m1, r2, m2;
-        cin >> r1 >> m1 >> r2 >> m2;
-        auto ans = CRT(r1,m1,r2,m2);
-        if (ans.first == -1) cout << "no solution\n";
-        else cout << ans.first << " " << ans.second << '\n';
+// ==============================================
+// CRT for a system of N modular linear equations
+// ==============================================
+//  Args:
+//      r = array of remainders
+//      m = array of modules
+//      n = length of both arrays
+//  Output: 
+//      a pair {X, lcm} where X is the solution of the sytemm
+//          X = r[i] (mod m[i]) for i = 0 ... n-1
+//      and lcm = LCM(m[0], m[1], ..., m[n-1])
+//      if there is no solution, the output is {-1, -1}
+// ** Note: this solution works if LCM(m[0],...,m[n-1]) fits in a long long (64 bits)
+pair<ll,ll> CRT(ll* r, ll* m, int n) {
+    ll r1 = r[0], m1 = m[0];
+    rep(i,1,n-1) {
+        ll r2 = r[i], m2 = m[i];
+        ll g, x, y; xgcd(m1, m2, g, x, y);
+        if ((r1 - r2) % g != 0) return {-1, -1}; // no solution
+        ll z = m2/g;
+        ll lcm = m1 * z;
+        ll sol = add(mod(r1, lcm), m1*mult(mod(x,z),mod((r2-r1)/g,z),z), lcm);
+        r1 = sol;
+        m1 = lcm;
     }
-    return 0;
+    // for debugging (in case you think you might have bugs)
+    // assert (0 <= r1 and r1 < m1);
+    // rep(i,0,n-1) assert (r1 % m[i] == r[i]);
+    return {r1, m1};
 }
