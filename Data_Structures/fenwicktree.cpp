4e50ac3602ffd60c770956c3a8c13ae7a954ac76
PabloMessina <pamessina@uc.cl>
2019-08-18 16:39:10 -0400

    update

--- Data_Structures/fenwicktree.cpp
+++ Data_Structures/fenwicktree.cpp
@@ -1,10 +1,10 @@
-struct FenwickTree {
-    vector<int> ft;
-    FenwickTree(int n) { ft.assign(n+1, 0); }
+struct BIT { // BIT = binary indexed tree (a.k.a. Fenwick Tree)
+    vector<int> bit;
+    BIT(int n) { bit.assign(n+1, 0); }
     // prefix sum query (sum in range 1 .. k)
     int psq(int k) {
         int sum = 0;
-        for (; k; k -= (k & -k)) sum += ft[k];
+        for (; k; k -= (k & -k)) sum += bit[k];
         return sum;
     }
     // range sum query (sum in range a .. b)
@@ -13,10 +13,6 @@ struct FenwickTree {
     }
     // increment k'th value by v (and propagate)
     void add(int k, int v) {
-        for (; k < ft.size(); k += (k & -k)) ft[k] += v;
-    }
-    // increment range [i ... j] with v (and propagate)
-    void range_add(int i, int j, int v) {
-        add(i, v); add(j+1, -v);
+        for (; k < bit.size(); k += (k & -k)) bit[k] += v;
     }
 };
