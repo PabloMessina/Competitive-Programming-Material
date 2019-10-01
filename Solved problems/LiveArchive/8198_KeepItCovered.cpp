ca548f5982e08dc354a7b094fbaf154888235fa4
PabloMessina <pamessina@uc.cl>
2019-10-01 13:49:23 -0300

    update

--- Solved problems/LiveArchive/8198_KeepItCovered.cpp
+++ Solved problems/LiveArchive/8198_KeepItCovered.cpp
@@ -1,36 +1,21 @@
 // tags: bipartite matching, maxflow, dinic
-#include <bits/stdc++.h> // import everything in one shot
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
 using namespace std;
-// defines
 #define rep(i,a,b) for(int i = a; i <= b; ++i)
-#define invrep(i,b,a) for(int i = b; i >= a; --i)
 #define umap unordered_map
-#define uset unordered_set
-// typedefs
-typedef unsigned int uint;
-typedef unsigned long long int ull;
 typedef long long int ll;
-typedef vector<int> vi;
-typedef pair<int,int> ii;
-typedef tuple<int,int,int> iii;
 // -------------------------------
-
-const int MAXR = 20;
-int R,C,N;
-char board[MAXR][MAXR+1];
-
-namespace ID_MAPPER {
-    umap<int,int> x2id;
-    int ID;
-    void reset() {
-        x2id.clear();
-        ID = 0;
+int R, C;
+vector<string> board;
+int ID;
+umap<int,int> x2id;
+int get_id(int x) {
+    auto it = x2id.find(x);
+    if (it == x2id.end()) {
+        return x2id[x] = ID++;
     }
-    int get_id(int x) {
-        auto it = x2id.find(x);
-        if (it == x2id.end()) return x2id[x] = ID++;
-        return it->second;
-    }
+    return it->second;
 }
 
 struct Dinic {
@@ -89,7 +74,7 @@ struct Dinic {
  
     void add_edge(int u, int v, ll cap) {
         edge a = {v, (int)g[v].size(), 0, cap};
-        edge b = {u, (int)g[u].size(), 0, 0};
+        edge b = {u, (int)g[u].size(), 0, 0}; //Poner cap en vez de 0 si la arista es bidireccional
         g[u].push_back(a);
         g[v].push_back(b);
     }
@@ -106,93 +91,45 @@ struct Dinic {
     }
 };
 
-const int dirs[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+int movr[4] = {0, 1, 0, -1};
+int movc[4] = {1, 0, -1, 0};
 
 int main() {
-    main_loop:
-    while (scanf("%d%d", &R, &C) == 2) {
-        int even_count = 0;
-        int odd_count = 0;
-        rep(r,0,R-1) {
-            scanf("%s", board[r]);
-            rep(c,0,C-1) {
-                if (board[r][c] == 'o') {
-                    if ((r+c)%2==0) even_count++;
-                    else odd_count++;
-                } else {
-                    if ((r+c)%2==0) even_count+=2;
-                    else odd_count+=2;
+    ios::sync_with_stdio(false); 
+    cin.tie(0); cout.tie(0);
+    while (cin >> R >> C) {
+        board.resize(R);
+        ID = 0; x2id.clear();
+        Dinic din(R * C + 2);
+        int s = get_id(8888);
+        int t = get_id(9999);
+        int tot_cap_in = 0;
+        int tot_cap_out = 0;
+        rep(r,0,R-1) cin >> board[r];
+        rep(r,0,R-1) rep(c,0,C-1) {
+            int u = get_id(r * C + c);
+            int cap = 1 + (board[r][c] == '-');
+            if ((r + c) % 2 == 0) {
+                din.add_edge(s, u, cap);
+                tot_cap_in += cap;
+                rep(i,0,3) {
+                    int rr = r + movr[i];
+                    int cc = c + movc[i];
+                    if (0 <= rr and rr < R and 0 <= cc and cc < C) {
+                        int v = get_id(rr * C + cc);
+                        din.add_edge(u, v, cap);
+                    }
                 }
+            } else {
+                din.add_edge(u, t, cap);
+                tot_cap_out += cap;
             }
         }
-        if (even_count != odd_count) {
-            puts("N");
-            goto main_loop;
+        if (tot_cap_in == tot_cap_out and din.max_flow(s, t) == tot_cap_in) {
+            cout << "Y\n";
+        } else {
+            cout << "N\n";
         }
-        N = even_count + odd_count + 2;
-        ID_MAPPER::reset();
-        int source = ID_MAPPER::get_id(88888);
-        int target = ID_MAPPER::get_id(99999);
-        Dinic din(N);
-        rep(r,0,R-1) {
-            rep(c,0,C-1) {
-                if (board[r][c] == 'o') {
-                    int u = ID_MAPPER::get_id(r * C + c);
-                    if ((r+c) % 2 == 0) {
-                        din.add_edge(source, u, 1);
-                        rep(i,0,3) {
-                            int rr = r + dirs[i][0];
-                            int cc = c + dirs[i][1];
-                            if (0 <= rr and rr < R and 0 <= cc and cc < C) {
-                                if (board[rr][cc] == 'o') {
-                                    int v = ID_MAPPER::get_id(C * rr + cc);
-                                    din.add_edge(u, v, 1);
-                                } else {
-                                    int v1 = ID_MAPPER::get_id(C * rr + cc);
-                                    int v2 = ID_MAPPER::get_id(R*C + C * rr + cc);
-                                    din.add_edge(u, v1, 1);
-                                    din.add_edge(u, v2, 1);
-                                }
-                            }
-                        }
-                    } else {
-                        din.add_edge(u, target, 1);
-                    }                    
-                } else {
-                    int u1 = ID_MAPPER::get_id(r * C + c);
-                    int u2 = ID_MAPPER::get_id(R*C + r * C + c);
-                    if ((r+c) % 2 == 0) {
-                        din.add_edge(source, u1, 1);
-                        din.add_edge(source, u2, 1);
-                        rep(i,0,3) {
-                            int rr = r + dirs[i][0];
-                            int cc = c + dirs[i][1];
-                            if (0 <= rr and rr < R and 0 <= cc and cc < C) {
-                                if (board[rr][cc] == 'o') {
-                                    int v = ID_MAPPER::get_id(C * rr + cc);
-                                    din.add_edge(u1, v, 1);
-                                    din.add_edge(u2, v, 1);
-                                } else {
-                                    int v1 = ID_MAPPER::get_id(C * rr + cc);
-                                    int v2 = ID_MAPPER::get_id(R*C + C * rr + cc);
-                                    din.add_edge(u1, v1, 1);
-                                    din.add_edge(u1, v2, 1);
-                                    din.add_edge(u2, v1, 1);
-                                    din.add_edge(u2, v2, 1);
-                                }
-                            }
-                        }
-                    } else {
-                        din.add_edge(u1, target, 1);
-                        din.add_edge(u2, target, 1);
-                    }                    
-                }
-            }
-        }
-        if (din.max_flow(source, target) == even_count)
-            puts("Y");
-        else
-            puts("N");
     }
     return 0;
 }
