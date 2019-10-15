b3eb75ab2eb7a37203c8f15309cb767d57b1a50a
PabloMessina <pamessina@uc.cl>
2019-10-14 22:13:35 -0300

    update

--- Strings/suffix_array.cpp
+++ Strings/suffix_array.cpp
@@ -24,20 +24,21 @@ struct SuffixArray {
         invrep(i,n-1,0) sa_tmp[--counts[get_rank(sa[i]+k)]] = sa[i];
         sa.swap(sa_tmp);
     }
-    void compute_sa(vector<int>& s, int maxv) {        
-        rep(i,0,n-1) sa[i] = i, rank[i] = s[i];
+    void compute_sa(vector<int>& s) {
+        rep(i,0,n-1) sa[i] = i;
+        sort(sa.begin(), sa.end(), [&s](int i, int j) { return s[i] < s[j]; });
+        int r = rank[sa[0]] = 1;
+        rep(i,1,n-1) rank[sa[i]] = (s[sa[i]] != s[sa[i-1]]) ? ++r : r;
         for (int h=1; h < n; h <<= 1) {
-            counting_sort(maxv, h);
-            counting_sort(maxv, 0);
-            int r = 1;
-            rank_tmp[sa[0]] = r;
+            counting_sort(r, h);
+            counting_sort(r, 0);
+            r = rank_tmp[sa[0]] = 1;
             rep(i,1,n-1) {
                 if (rank[sa[i]] != rank[sa[i-1]] or
-                    get_rank(sa[i]+h) != get_rank(sa[i-1]+h)) r++;
+                    get_rank(sa[i]+h) != get_rank(sa[i-1]+h)) ++r;
                 rank_tmp[sa[i]] = r;
             }
             rank.swap(rank_tmp);
-            maxv = r;
         }
     }
     // LCP construction in O(N) using Kasai's algorithm
@@ -56,11 +57,9 @@ struct SuffixArray {
     }
     SuffixArray(vector<int>& s) {
         n = s.size();
-        if (n == 1) s[0] = 1; // prevent bugs
         rank.resize(n); rank_tmp.resize(n);
         sa.resize(n); sa_tmp.resize(n);
-        int maxv = *max_element(s.begin(), s.end());
-        compute_sa(s, maxv);
+        compute_sa(s);
         compute_lcp(s); // optional: only if lcp array is needed
     }
 };
@@ -68,7 +67,7 @@ struct SuffixArray {
 int main() { // how to use
     string test; cin >> test;
     vector<int> s;
-    for (char c : test) s.push_back(c - 'a' + 1); // make sure all values are >= 1
+    for (char c : test) s.push_back(c);
     SuffixArray sa(s);
     for (int i : sa.sa) cout << i << ":\t" << test.substr(i) << '\n';
     rep (i, 0, s.size() - 1) {
