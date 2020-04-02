b8680fa719139e2308c7202ab3512a3f30288c2f
PabloMessina <pamessina@uc.cl>
2020-04-02 20:57:15 -0300

    update

--- Solved problems/LiveArchive/5822_DistributingBallotBoxes_v2.cpp
+++ Solved problems/LiveArchive/5822_DistributingBallotBoxes_v2.cpp
@@ -1,7 +1,7 @@
 // tags: binary search
-#include <bits/stdc++.h> // import everything in one shot
+#include <bits/stdc++.h>
 using namespace std;
-#define rep(i,a,b) for(int i = a; i <= b; ++i)
+#define rep(i,a,b) for(int i = a; i < b; ++i)
 // -------------------------------
 int pop[500000];
 int main() {
@@ -10,12 +10,12 @@ int main() {
     while (true) {
         int N, B; cin >> N >> B;
         if (N==-1) break;
-        rep(i,0,N-1) cin >> pop[i];
+        rep(i,0,N) cin >> pop[i];
         int l = 1, r = *max_element(pop, pop+N);
         while (l < r) {
             int m = (l+r) >> 1;
             int c = B;
-            rep(i,0,N-1) {
+            rep(i,0,N) {
                 c -= ((pop[i] / m) + (pop[i] % m > 0));
                 if (c < 0) break;
             }
