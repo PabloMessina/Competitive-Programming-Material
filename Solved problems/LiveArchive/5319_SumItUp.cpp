117e19c59edf581f9017bec499997ed09b6fbea5
PabloMessina <pamessina@uc.cl>
2020-04-18 21:58:54 -0400

    update

--- Solved problems/LiveArchive/5319_SumItUp.cpp
+++ Solved problems/LiveArchive/5319_SumItUp.cpp
@@ -1,14 +1,9 @@
+// tags: backtracking, implementation
 #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
-// defines
 #define rep(i,a,b) for(int i = a; i < b; ++i)
 #define invrep(i,b,a) for(int i = b; i >= a; --i)
-#define umap unordered_map
-#define uset unordered_set
-// typedefs
-typedef unsigned long long int ull;
-typedef long long int ll;
 // -------------------------------
 int N, nums[12], counts[12], counts_aux[12];
 bool solve(int i, int sum) {
