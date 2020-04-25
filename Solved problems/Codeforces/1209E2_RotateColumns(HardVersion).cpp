4d00a9ad7575a494bfc2f78f811fab7152c6ecb5
PabloMessina <pamessina@uc.cl>
2020-04-24 22:12:38 -0400

    update

--- Solved problems/Codeforces/1209E2_RotateColumns(HardVersion).cpp
+++ Solved problems/Codeforces/1209E2_RotateColumns(HardVersion).cpp
@@ -2,7 +2,7 @@
 #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,a,b) for(int i = a; i < b; ++i)
+#define rep(i,a,b) for(int i = a; i <= b; ++i)
 // -------------------------------
 int N, M;
 struct Column {
@@ -14,16 +14,16 @@ struct Column {
     bool operator<(const Column& o) const { return maxv > o.maxv; }
 };
 vector<Column> cols;
-
+ 
 int submasks[1 << 12][1 << 12];
 void precompute_submasks() {
-    rep(mask,0,(1<<12)) {
+    rep(mask,0,(1<<12)-1) {
         vector<int> bit_idxs;
         for (int i=0, tmp=mask; tmp > 0; ++i, tmp>>=1) {
             if (tmp & 1) bit_idxs.push_back(i);
         }        
         int xmax = (1<<bit_idxs.size())-1;
-        rep(x, 0, xmax+1) {
+        rep(x, 0, xmax) {
             int submask = 0;
             for (int i=0, tmp=x; tmp > 0; ++i, tmp>>=1) {
                 if (tmp & 1) submask |= (1 << bit_idxs[i]);
@@ -32,31 +32,31 @@ void precompute_submasks() {
         }
     }
 }
-
+ 
 int mask2class[1 << 12];
-void precompute_mask_equivalence_classes() {
-    rep(mask, 0, (1 << N)) {
+void precompute_equivalence_mask_classes() {
+    rep(mask, 0, (1 << N)-1) {
         int mask_class = mask;
         int tmp = mask;
-        rep(i,2,N+1) {
+        rep(i,2,N) {
             tmp = (tmp >> 1) | ((tmp & 1) << (N-1));
             mask_class = min(mask_class, tmp);
         }
         mask2class[mask] = mask_class;
     }
 }
-
+ 
 int maxsum[12][1 << 12];
 void precompute_maxsum() {
     memset(maxsum, 0, sizeof maxsum);
-    rep(i,0,M) rep(mask,0,(1<<N)) {
+    rep(i,0,M-1) rep(mask,0,(1<<N)-1) {
         int mclass = mask2class[mask];
         int sum = 0;
-        rep(j,0,N) if ((mask >> j) & 1) sum += cols[i][j];
+        rep(j,0,N-1) if ((mask >> j) & 1) sum += cols[i][j];
         maxsum[i][mclass] = max(maxsum[i][mclass], sum);
     }
 }
-
+ 
 vector<int> memo;
 int dp(int i, int mask) {
     int& ans = memo[mask * M + i];
@@ -65,7 +65,7 @@ int dp(int i, int mask) {
         ans = maxsum[i][mask2class[mask]];
     } else {
         ans = 0;
-        int xmax = 1 << __builtin_popcount(mask);
+        int xmax = (1 << __builtin_popcount(mask))-1;
         rep(x,0,xmax) {
             int submask = submasks[mask][x];
             ans = max(ans, maxsum[i][mask2class[submask]] + dp(i+1, mask - submask));
@@ -73,7 +73,7 @@ int dp(int i, int mask) {
     }
     return ans;
 }
-
+ 
 int main() {
     ios::sync_with_stdio(false); 
     cin.tie(0); cout.tie(0);
@@ -96,7 +96,7 @@ int main() {
             cols.resize(N);
             M = N;
         }
-        precompute_mask_equivalence_classes();
+        precompute_equivalence_mask_classes();
         precompute_maxsum();
         memo.assign((1 << N) * M, -1);
         cout << dp(0, (1<<N)-1) << '\n';
