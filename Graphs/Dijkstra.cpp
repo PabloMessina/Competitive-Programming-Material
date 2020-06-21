cdf3745f618a904bea1c6185e3c370db58e17b3c
PabloMessina <pamessina@uc.cl>
2020-06-20 23:26:31 -0400

    update

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -8,14 +8,14 @@ vector<vector<ii>> g; // graph
 int N; // number of nodes
 vector<int> mindist; // min distance from source to each node
 vector<int> parent; // parent of each node in shortest path from source
-void dijkstra(int source) {
+void dijkstra(int s) {
     parent.assign(N, -1);
     mindist.assign(N, INT_MAX);
-    mindist[source] = 0;
+    mindist[s] = 0;
     priority_queue<ii, vector<ii>, greater<ii>> q; // minheap
-    q.emplace(0, source);
+    q.emplace(0, s);
     while (q.size()) {
-        int d, u; tie(d, u) = q.top(); q.pop(); // u = node, d = mindist from source to u
+        int d, u; tie(d, u) = q.top(); q.pop(); // u = node, d = mindist from s to u
         if (mindist[u] < d) continue; // skip outdated improvements
         for (auto& e : g[u]) {
             if (mindist[e.ff] > d + e.ss) {
---
+++ Solved problems/SPOJ/SEGMENTS.cpp
@@ -0,0 +1,71 @@
+// tags: math, inequalities, graphs, bellman ford, shortest path, negative cycle,
+// implementation, binary search
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+// defines
+#define rep(i,a,b) for(int i = a; i < b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+#define umap unordered_map
+#define uset unordered_set
+#define ff first
+#define ss second
+// typedefs
+typedef pair<int,int> ii;
+typedef unsigned long long int ull;
+typedef long long int ll;
+// -------------------------------
+bool bellmanford(vector<vector<ii>>& g, int s) {
+    int n = g.size();
+    vector<int> d(n, INT_MAX);
+    d[s] = 0;
+    rep(_,1,n) rep(u,0,n) if (d[u] < INT_MAX) for (auto& e : g[u]) {
+        d[e.ff] = min(d[e.ff], d[u] + e.ss);
+    }
+    rep(u,0,n) if (d[u] < INT_MAX) for (auto& e : g[u]) {
+        if (d[e.ff] > d[u] + e.ss) return true;
+    }
+    return false;
+}
+bool possible(vector<pair<ll,ll>>& segs, int n, int r) {
+    vector<vector<ii>> g(n+1);
+    for (auto& p : segs) {
+        int u, v; tie(u,v) = p;
+        g[u].emplace_back(v, r);
+        g[v].emplace_back(u, -1);
+    }
+    rep(u,1,n) g[u].emplace_back(u-1, 0);
+    int s = n;
+    rep(u,0,n) g[s].emplace_back(u, 0);
+    return not bellmanford(g, s);
+}
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    int N; cin >> N;
+    vector<pair<ll,ll>> segs;
+    set<ll> xset;
+    rep(i,0,N) {
+        ll a, b, h; cin >> a >> b >> h;
+        a = 1000 * a + 1;
+        b = 1000 * b - 1;
+        segs.emplace_back(a, b);
+        xset.insert(a);
+        xset.insert(b);
+    }
+    umap<ll,int> x2id;
+    int id = 0;
+    for (ll x : xset) x2id[x] = id++;
+    for (auto& p : segs) {
+        p.ff = x2id[p.ff];
+        p.ss = x2id[p.ss];
+    }
+    int l = 1, r = N;
+    while (l < r) {
+        int m = (l+r)>>1;
+        if (possible(segs, id, m)) r = m;
+        else l = m+1;
+    }
+    cout << l << '\n';
+    return 0;
+}
