5079318c603a6d732bef04ed8d4ec4b2e5b62447
PabloMessina <pamessina@uc.cl>
2020-06-20 23:26:31 -0400

    update

--- Graphs/BellmanFord.cpp
+++ Graphs/BellmanFord.cpp
@@ -7,10 +7,8 @@ vector<int> bellman_ford(vector<vector<ii>>& g, int s, bool& neg_cycle) {
     int n = g.size();
     vector<int> d(n, INT_MAX); // store min distance from s to all nodes
     d[s] = 0;
-    rep(_,1,n) {
-        rep(u,0,n) if (d[u] < INT_MAX) for (auto& e : g[u]) {
-            d[e.ff] = min(d[e.ff], d[u] + e.ss);
-        }
+    rep(_,1,n) rep(u,0,n) if (d[u] < INT_MAX) for (auto& e : g[u]) {
+        d[e.ff] = min(d[e.ff], d[u] + e.ss);
     }
     // optional: detect negative cycle
     rep(u,0,n) if (d[u] < INT_MAX) for (auto& e : g[u]) {
