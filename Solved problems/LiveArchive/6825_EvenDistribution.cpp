e6f8d02ef29faa24dc05a12493c7df671d0ce270
PabloMessina <pamessina@uc.cl>
2020-06-29 12:18:37 -0400

    update

--- Solved problems/LiveArchive/6825_EvenDistribution.cpp
+++ Solved problems/LiveArchive/6825_EvenDistribution.cpp
@@ -1,32 +1,41 @@
 // tags: BFS, GCD
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,a,b) for(int i=a; i<=b; i++)
-#define uset unordered_set 
+// defines
+#define rep(i,a,b) for(int i = a; i < b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+#define umap unordered_map
+#define uset unordered_set
+#define ff first
+#define ss second
+// typedefs
 typedef pair<int,int> ii;
-
-#define MAXI 10000
-#define MAXC 100000
-
+typedef unsigned long long int ull;
+typedef long long int ll;
+// -------------------------------
+const int MAXI = 10000;
+const int MAXC = 100000;
 int I,S;
 int candies[MAXI];
 vector<vector<int>> g;
 uset<int> found_nums;
 set<ii> states;
-
 int main() {
-    while (scanf("%d%d",&I,&S)==2) {
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    while (cin >> I >> S) {
         g.assign(I, vector<int>());
-        rep(i,0,I-1) scanf("%d", &candies[i]);
-        rep(i,1,S) {
-            int a, b; scanf("%d%d",&a,&b); --a, --b;
+        rep(i,0,I) cin >> candies[i];
+        rep(i,1,S+1) {
+            int a, b; cin >> a >> b; --a, --b;
             g[a].push_back(b);
             g[b].push_back(a);
         }
         queue<ii> q;        
         found_nums.clear();
         states.clear();
-        rep(i,0,I-1) {
+        rep(i,0,I) {
             ii state(i, candies[i]);
             q.push(state);
             states.insert(state);
@@ -45,7 +54,7 @@ int main() {
                 }
             }
         }
-        printf("%d\n", (int)found_nums.size());
+        cout << found_nums.size() << '\n';
     }
     return 0;
 }
