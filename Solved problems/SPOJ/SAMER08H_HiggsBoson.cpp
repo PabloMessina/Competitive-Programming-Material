31b8b8f7b4b0bc116bedbc541fc4fe9e01114a7e
PabloMessina <pamessina@uc.cl>
2020-07-16 17:18:55 -0400

    update

--- Solved problems/SPOJ/SAMER08H_HiggsBoson.cpp
+++ Solved problems/SPOJ/SAMER08H_HiggsBoson.cpp
@@ -74,59 +74,61 @@ int main() {
         if (not (a1 or b1 or c1 or d1 or a2 or b2 or c2 or d2)) break;
 
         Fraction t_min = {INT_MAX, 1};
-        if (a1 == a2 and b1 == b2) {
-            if (c1 == c2 and d1 == d2) {
-                update_ans(t_min, {0, 1});
-            } else {
-                if ((a1 == 0 and b1 == 0) or (a1 != 0)) {
-                    update_ans(t_min, {-b1, a1 != 0 ? a1 : 1});
+        if (a1 == a2 and b1 == b2) { // same radius equation
+            if (c1 == c2 and d1 == d2) { // same angle equation
+                update_ans(t_min, {0, 1}); // collision at time 0
+            } else { // diferent angle equations
+                if ((a1 == 0 and b1 == 0) or (a1 != 0)) { // if there is an instant when the radius becomes 0
+                    update_ans(t_min, {-b1, a1 != 0 ? a1 : 1}); // in that instant there is collision (angle doesn't matter)
                 }
-                if (c1 == c2) {
+                // find smallest t >= 0 such that
+                //  c1 * t + d1 == c2 * t + d2 + 360*k
+                //  (c1 - c2) * t == (d2 - d1) + 360*k
+                // where k must be integer
+                if (c1 == c2) { // case 1: c1 == c2
                     if ((d2 - d1) % 360 == 0) update_ans(t_min, ZERO);
-                } else if (c1 - c2 > 0) {
+                } else if (c1 > c2) { // case 2: c1 > c2 => (d2 - d1) + 360 * k >= 0
                     ll k = discrete_ceil(d1 - d2, 360);
-                    assert (d2 - d1 + 360 * k >= 0);
-                    assert (d2 - d1 + 360 * (k-1) < 0);
                     update_ans(t_min, {d2 - d1 + 360 * k, c1 - c2});
-                } else {
+                } else { // case 3: c1 < c2 => (d2 - d1) + 360 * k < 0
                     ll k = discrete_floor(d1 - d2, 360);
-                    assert (d2 - d1 + 360 * k <= 0);
-                    assert (d2 - d1 + 360 * (k+1) > 0);
                     update_ans(t_min, {d2 - d1 + 360 * k, c1 - c2});
                 }
             }
-        } else {            
-            {
+        } else { // different radius equations
+            { // case 1 : when r1(t) and r2(t) intersect in a single point
                 Fraction t(b2 - b1, a1 - a2);
                 if (t.b != 0) {
-                    if (t * a1 + b1 == ZERO) update_ans(t_min, t);
-                    else {
+                    if (t * a1 + b1 == ZERO) update_ans(t_min, t); // r1 = r2 = 0  => angle doesn't matter
+                    else { // r1 = r2 != 0  => angle matters
                         Fraction f = (t * c1 + d1) - (t * c2 + d2);
                         f.normalize();
                         if (f.b == 1 and (f.a % 360) == 0) update_ans(t_min, t);
                     }
                 }
             }
+            // case 2: r1(t) = -r2(t)
             if (a1 == -a2 and b1 == -b2) {
-                if (c1 == c2) {
+                // find smallest t >= 0 such that
+                //  c1 * t + d1 == c2 * t + d2 + 180 + 360*k
+                //  (c1 - c2) * t == (d2 - d1) + 180 + 360*k
+                // where k must be integer
+                if (c1 == c2) { // case 2.1: c1 == c2 
                     if ((d2 - d1 + 180) % 360 == 0) update_ans(t_min, ZERO);
-                } else if (c1 - c2 > 0) {
+                } else if (c1 > c2) { // case 2.2: c1 > c2 => (d2 - d1) + 180 + 360 * k >= 0
                     ll k = discrete_ceil(d1 - d2 - 180, 360);
-                    assert (d2 - d1 + 180 + 360 * k >= 0);
-                    assert (d2 - d1 + 180 + 360 * (k-1) < 0);
                     update_ans(t_min, {d2 - d1 + 180 + 360 * k, c1 - c2});
-                } else {                    
+                } else { // case 2.3: c1 < c2 => (d2 - d1) + 180 + 360 * k < 0
                     ll k = discrete_floor(d1 - d2 - 180, 360);
-                    assert (d2 - d1 + 180 + 360 * k <= 0);
-                    assert (d2 - d1 + 180 + 360 * (k+1) > 0);
                     update_ans(t_min, {d2 - d1 + 180 + 360 * k, c1 - c2});
                 }
             }
+            // case 3: when r1(t) intersects -r2(t) in a single point
             {
                 Fraction t(-b2 - b1, a1 + a2);
                 if (t.b != 0) {
-                    if (t * a1 + b1 == ZERO) update_ans(t_min, t);
-                    else {
+                    if (t * a1 + b1 == ZERO) update_ans(t_min, t); // r1(t) == -r2(t) == 0 => angle doesn't matter
+                    else { // r1(t) == -r2(t) != 0  =>  angle matters
                         Fraction f = (t * c1 + d1) - (t * c2 + d2);
                         f.normalize();
                         if (f.b == 1 and ((f.a + 180) % 360) == 0) update_ans(t_min, t);
