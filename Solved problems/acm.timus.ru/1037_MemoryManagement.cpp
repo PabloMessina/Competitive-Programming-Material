65e149e55efff3c74ab79538fa593dbcf32770b1
PabloMessina <pamessina@uc.cl>
2020-08-21 21:25:05 -0400

    update

--- Solved problems/acm.timus.ru/1037_MemoryManagement.cpp
+++ Solved problems/acm.timus.ru/1037_MemoryManagement.cpp
@@ -1,69 +1,56 @@
-#include <string>
-#include <sstream>
-#include <vector>
-#include <iostream>
-#include <cstdlib>
-#include <queue>
-#include <cstdio>
+// tags: implementation, simulation
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
 using namespace std;
-
-#define FOR(i,i0,in) for(int i = i0; i <= in; ++i)
-
-struct Busy {
-  int index;
-  int deadline;
-  Busy(int i, int d) : index(i), deadline(d) {}
+#define rep(i,a,b) for(int i = a; i < b; ++i)
+enum { ALLOC, ACCESS, FREE };
+struct Event {
+    int t, id, type, block;
+    bool operator<(const Event& o) const { return tie(t, id) > tie(o.t, o.id); }
 };
-
-void split(const string &s, char delim, vector<string> &elems) {
-  stringstream ss(s);
-  string item;
-  while (getline(ss, item, delim)) {
-    elems.push_back(item);
-  }
-}
-
+int N = 30000, T = 10 * 60;
 int main() {
-  string line;
-  vector<int> deadlines(30001, -1);
-  priority_queue<int, vector<int>, greater<int> > frees;
-  FOR(i,1,30000)
-    frees.push(i);
-  queue<Busy> busies;
-
-  while (getline(cin, line)) {
-    vector<string> tokens;
-    split(line, ' ', tokens);
-
-    int time = atoi(tokens[0].c_str());
-    while (!busies.empty()) {
-      Busy front = busies.front();
-      if (front.deadline <= time) {
-        busies.pop();
-        if (front.deadline == deadlines[front.index]) {
-          frees.push(front.index);
-          deadlines[front.index] = -1;
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    priority_queue<Event> events;
+    priority_queue<int, vector<int>, greater<int>> free_blocks;
+    vector<bool> isfree(N, true);
+    vector<int> free_time(N, -1);
+    rep(i,0,N) free_blocks.push(i);
+    string line;
+    int id = 0;
+    while (getline(cin, line)) {
+        stringstream ss(line);
+        int t; char op; ss >> t >> op;
+        if (op == '+') {
+            events.push({t, id, ALLOC, -1});
+        } else {
+            int block; ss >> block; --block;
+            events.push({t, id, ACCESS, block});
         }
-      } else {
-        break;
-      }
+        ++id;
     }
-    if (tokens.size() == 2) { // allocate
-      int f = frees.top(); frees.pop();
-      int t = time + 600;
-      busies.push(Busy(f, t));
-      deadlines[f] = t;
-      printf("%d\n", f);
-    } else { // access
-      int address = atoi(tokens[2].c_str());
-      if (deadlines[address] == -1) puts("-");
-      else {
-        puts("+");
-        int t = time + 600;
-        deadlines[address] = t;
-        busies.push(Busy(address, t));
-      }
+    while (events.size()) {
+        Event e = events.top(); events.pop();
+        if (e.type == ACCESS) {
+            if (!isfree[e.block]) {
+                cout << "+\n";
+                free_time[e.block] = e.t + T;
+                events.push({e.t + T, -1, FREE, e.block});
+            } else {
+                cout << "-\n";
+            }
+        } else if (e.type == ALLOC) {
+            int block = free_blocks.top();
+            free_blocks.pop();
+            free_time[block] = e.t + T;
+            isfree[block] = false;
+            events.push({e.t + T, -1, FREE, block});
+            cout << (block + 1) << '\n';
+        } else if (free_time[e.block] == e.t and !isfree[e.block]) {
+            isfree[e.block] = true;
+            free_blocks.push(e.block);
+        }
     }
-  }
-  return 0;
+    return 0;
 }
