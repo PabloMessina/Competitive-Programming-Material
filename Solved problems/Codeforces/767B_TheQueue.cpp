ef52d40448c4456a238a4e5fc1649f3ecdcf134d
PabloMessina <pamessina@uc.cl>
2020-08-28 23:59:42 -0400

    update

--- Solved problems/Codeforces/767B_TheQueue.cpp
+++ Solved problems/Codeforces/767B_TheQueue.cpp
@@ -9,14 +9,14 @@ int main() {
     cin.tie(0);
     ll ts, tf, t; int n;
     cin >> ts >> tf >> t >> n;
-    priority_queue<ll, vector<ll>, greater<ll>> q;
+    queue<ll> q;
     ll last_exit = ts;
     ll min_wait = LLONG_MAX;
     ll ans = ts;
     ll prev_x = 0;
     while (n--) {
-        ll x; cin >> x;
-        while (q.size() and q.top() < x) q.pop();
+        ll x; cin >> x; if (x >= tf) break;
+        while (q.size() and q.front() < x) q.pop();
         if (prev_x < x) {
             if (q.empty()) {
                 ll s = max(x-1, ts);
@@ -26,14 +26,14 @@ int main() {
                     ans = x-1;
                 }
             } else {
-                ll wait = (q.top() - x + 1) + (q.size() - 1) * t;
+                ll wait = (q.front() - x + 1) + (q.size() - 1) * t;
                 if (wait < min_wait and x-1 + wait + t <= tf) {
                     min_wait = wait;
                     ans = x-1;
                 }
             }
         }
-        while (q.size() and q.top() <= x) q.pop();
+        while (q.size() and q.front() <= x) q.pop();
         last_exit = max(last_exit, x) + t;
         q.push(last_exit);
         prev_x = x;
