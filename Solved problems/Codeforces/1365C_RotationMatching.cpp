7621c124a29499e2853c9e866f994de86f4f4441
PabloMessina <pamessina@uc.cl>
2020-08-29 21:24:19 -0400

    update

--- Solved problems/Codeforces/1365C_RotationMatching.cpp
+++ Solved problems/Codeforces/1365C_RotationMatching.cpp
@@ -1,18 +1,8 @@
-// tags: implementation
+// tags: implementation, modular arithmetic, math
 #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
-// defines
 #define rep(i,a,b) for(int i = a; i < b; ++i)
-#define invrep(i,b,a) for(int i = b; i >= a; --i)
-#define umap unordered_map
-#define uset unordered_set
-#define ff first
-#define ss second
-// typedefs
-typedef unsigned long long int ull;
-typedef long long int ll;
-
 int main() {
     ios::sync_with_stdio(false); 
     cin.tie(0); cout.tie(0);
@@ -29,11 +19,9 @@ int main() {
     int ans = 0;
     map<int,int> counts;
     rep(i,1,n+1) {
-        int d1 = (n + am[i] - bm[i]) % n;
-        int d2 = d1 - n;
-        counts[d1]++;
-        counts[d2]++;
-        ans = max({ans, counts[d1], counts[d2]});
+        int x = (n + am[i] - bm[i]) % n;
+        counts[x]++;
+        ans = max(ans, counts[x]);
     }
     cout << ans << '\n';
     return 0;
