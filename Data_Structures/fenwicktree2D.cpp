a5606a20cdb773f35de357680e0d3311b2d2905c
PabloMessina <pamessina@uc.cl>
2020-09-11 18:38:28 -0300

    update

--- Data_Structures/fenwicktree2D.cpp
+++ Data_Structures/fenwicktree2D.cpp
@@ -1,32 +1,32 @@
-struct BIT2D { // BIT = binary indexed tree (a.k.a. Fenwick Tree)
-    vector<int> bit;
+template<typename T> struct FT2D {
+    vector<vector<T>> ft;
     int R, C;
-    BIT2D(int _R, int _C) : R(_R+1), C(_C+1) {
-        bit.assign(R*C, 0);
+    FT2D(int _R, int _C) : R(_R+1), C(_C+1) {
+        ft.assign(R, vector<T>(C, 0));
     }
-    void add(int r, int c, int value) {
+    void add(int r, int c, T val) {
         for (int i = r; i < R; i += (i&-i))
             for (int j = c; j < C; j += (j&-j))
-                bit[i * C + j] += value;
+                ft[i][j] += val;
     }
     // sum[(1, 1), (r, c)]
-    int sum(int r, int c) {
-        int res = 0;
+    T sum(int r, int c) {
+        T res = 0;
         for (int i = r; i; i -= (i&-i))
             for (int j = c; j; j -= (j&-j))
-                res += bit[i * C + j];
+                res += ft[i][j];
         return res;
     }
     // sum[(r1, c1), (r2, c2)]
-    int sum(int r1, int c1, int r2, int c2) {
+    T sum(int r1, int c1, int r2, int c2) {
         return sum(r2, c2) - sum(r1-1, c2) - sum(r2, c1-1) + sum(r1-1, c1-1);
     }
     // get value at cell (r,c)
-    int get(int r, int c) {
+    T get(int r, int c) {
         return sum(r, c, r, c);
     }
     // set value to cell (r,c)
-    int set(int r, int c, int value) {
+    void set(int r, int c, int value) {
         add(r, c, -get(r, c) + value);
     }
 };
