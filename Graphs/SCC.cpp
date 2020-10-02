186e26904c71760a181ed22da02a873af8788551
PabloMessina <pamessina@uc.cl>
2020-10-02 18:43:11 -0300

    update

--- Graphs/SCC.cpp
+++ Graphs/SCC.cpp
@@ -1,60 +1,45 @@
 #include <bits/stdc++.h>
-#define rep(i,a,b) for(int i=a; i<=b; ++i)
+#define rep(i,a,b) for(int i=a; i<b; ++i)
 using namespace std;
-// -----------------------------------------
-// implementation of Tarjan's SCC algorithm
-struct tarjanSCC {
-    vector<int> _stack, ids, low;
-    vector<bool> instack;
-    vector<vector<int>>* g;
-    int n, ID;
-    void dfs(int u) {
-        ids[u] = low[u] = ID++;
-        instack[u] = true;
-        _stack.push_back(u);
-        for (int v : (*g)[u]) {
-            if (ids[v] == -1) {
-                dfs(v);
-                low[u] = min(low[v], low[u]);
-            } else if (instack[v]) {
-                low[u] = min(low[v], low[u]);
-            }
-        }
-        if (low[u] == ids[u]) {
-            // u is the root of a SCC
-            // ** here you can do whatever you want
-            // with the SCC just found
-            cout << "SCC found!\n";
-            // remove SCC from top of the stack
-            while (1) {
-                int x = _stack.back(); _stack.pop_back();
-                instack[x] = false;
-                if (x == u) break;
-            }
+// Tarjan's SCC algorithm
+vector<int> s, D, L;
+vector<bool> instack;
+vector<vector<int>> g;
+int n, ID;
+void dfs(int u) {
+    D[u] = L[u] = ID++;
+    instack[u] = true;
+    s.push_back(u);
+    for (int v : g[u]) {
+        if (D[v] == -1) {
+            dfs(v);
+            L[u] = min(L[v], L[u]);
+        } else if (instack[v]) {
+            L[u] = min(L[v], L[u]);
         }
     }
-    tarjanSCC(vector<vector<int>>& _g) {
-        g = &_g;
-        n = _g.size();
-        _stack.reserve(n);
-        ids.assign(n, -1);
-        low.resize(n);
-        instack.assign(n, 0);
-        ID = 0;
-        rep(u, 0, n-1) if (ids[u] == -1) dfs(u);
+    if (L[u] == D[u]) { // u is root of a SCC
+        cout << "SCC found!\n";        
+        while (1) { // remove SCC from top of the stack
+            int x = s.back(); s.pop_back();
+            instack[x] = false;
+            if (x == u) break;
+        }
     }
+}    
+void find_sccs(vector<vector<int>>& g) {
+    s.reserve(n); D.assign(n, -1); L.resize(n);
+    instack.assign(n, 0);
+    n = g.size(); ID = 0;
+    rep(u,0,n) if (ids[u] == -1) dfs(u);
 };
-
-// example of usage
 int main() {
-    // read and build graph from standard input
     int n, m; cin >> n >> m;
     vector<vector<int>> g(n);
     while(m--) {
         int u, v; cin >> u >> v; u--, v--;
         g[u].push_back(v);
     }
-    // find SCCs
-    tarjanSCC tscc(g);
+    find_sccs(g);
     return 0;
 }
--- Graphs/articulation-points,cut-edges,biconnected-components.cpp
+++ Graphs/articulation-points,cut-edges,biconnected-components.cpp
@@ -6,33 +6,32 @@
 //https://www.hackerearth.com/practice/algorithms/graphs/articulation-points-and-bridges/tutorial/
 //https://www.hackerearth.com/practice/algorithms/graphs/biconnected-components/tutorial/
 //http://web.iitd.ac.in/~bspanda/biconnectedMTL776.pdf
+#define ff first
+#define ss second
 typedef pair<int,int> ii;
-const int MAXN = 1000;
-int depth[MAXN];
-int low[MAXN];
-vector<int> g[MAXN];
-stack<ii> edge_stack;
+vector<int> D, L;
+vector<vector<int>> g;
+stack<ii> s; int root;
 
 void print_and_remove_bicomp(int u, int v) {
     puts("biconnected component found:");
     ii uv(u,v);
     while (true) {
-        ii top = edge_stack.top();
-        edge_stack.pop();
-        printf("(%d, %d)\n", top.first, top.second);
+        ii top = s.top(); s.pop();
+        printf("(%d, %d)\n", top.ff, top.ss);
         if (top == uv) break;
     }
 }
 
+// general version: find everything
 void dfs(int u, int p, int d) { // (node, parent, depth)
     static num_root_children = 0;    
-    depth[u] = d;
-    low[u] = d; // u at least can reach itself (ignoring u-p edge)
+    D[u] = L[u] = d; // u at least can reach itself (ignoring u-p edge)
     for(int v : g[u]) {
         if (v == p) continue; // direct edge to parent -> ignore
-        if (depth[v] == -1) { // exploring a new, unvisited child node            
-            edge_stack.emplace(u,v); // add edge to stack
-            dfs(v, u, d + 1); // explore recursively v's subtree
+        if (D[v] == -1) { // exploring a new, unvisited child node            
+            s.emplace(u,v); // add edge to stack
+            dfs(v, u, d+1); // explore recursively v's subtree
             // 1) detect articulation points and biconnected components
             if (p == -1) { // 1.1) special case: if u is root
                 if (++num_root_children == 2) {
@@ -43,21 +42,21 @@ void dfs(int u, int p, int d) { // (node, parent, depth)
                 // whenever we come back to the root, we just finished
                 // exploring a whole biconnected component
                 print_and_remove_bicomp(u,v);
-            } else if (low[v] >= d) { // 1.2) general case: non-root
+            } else if (L[v] >= d) { // 1.2) general case: non-root
                 printf("u = %d is articulation point\n",  u);
                 // we entered through and came back to an AP,
                 // so we just finished exploring a whole biconnected component
                 print_and_remove_bicomp(u,v);
             }
             // 2) detect cut edges (a.k.a. bridges)
-            if (low[v] > depth[u]) {
+            if (L[v] > D[u]) {
                 printf("(u,v) = (%d, %d) is cut edge\n", u, v);
             }
             // propagate low
-            low[u] = min(low[u], low[v]); 
-        } else if (depth[v] < d) { // back-edge to proper ancestor
-            edge_stack.emplace(u,v); // add edge to stack
-            low[u] = min(low[u], depth[v]); // propagate low
+            L[u] = min(L[u], L[v]); 
+        } else if (D[v] < d) { // back-edge to proper ancestor
+            s.emplace(u,v); // add edge to stack
+            L[u] = min(L[u], D[v]); // propagate low
         } else { // forward-edge to an already visited descendant
             // => do nothing, because this edge was already considered as a
             // back-edge from v -> u
@@ -65,3 +64,48 @@ void dfs(int u, int p, int d) { // (node, parent, depth)
     }
 }
 
+// find cut edges
+void dfs(int u, int p, int d) {
+    static num_root_children = 0;
+    D[u] = L[u] = d;
+    for(int v : g[u]) {
+        if (v == p) continue;
+        if (D[v] == -1) {
+            dfs(v, u, d+1);
+            if (L[v] > D[u]) printf("(u,v) = (%d, %d) is cut edge\n", u, v);
+            L[u] = min(L[u], L[v]); 
+        } else L[u] = min(L[u], D[v]);
+    }
+}
+
+// find articulation points
+int root_children = 0;
+void dfs(int u, int p, int d) {
+    D[u] = L[u] = d;
+    for(int v : g[u]) {
+        if (v == p) continue;
+        if (D[v] == -1) {
+            dfs(v, u, d+1);
+            if (p == -1) if (++root_children == 2) printf("root = %d is AP\n", root);
+            else if (L[v] >= d) printf("u = %d is AP\n",  u);
+            L[u] = min(L[u], L[v]); 
+        } else if (D[v] < d) L[u] = min(L[u], D[v]);
+    }
+}
+
+// find biconnected components
+void dfs(int u, int p, int d) {
+    D[u] = L[u] = d;
+    for(int v : g[u]) {
+        if (v == p) continue;
+        if (D[v] == -1) {
+            s.emplace(u,v);
+            dfs(v, u, d+1);
+            if (p == -1 or L[v] >= d) print_and_remove_bicomp(u,v);
+            L[u] = min(L[u], L[v]); 
+        } else if (D[v] < d) {
+            s.emplace(u,v);
+            L[u] = min(L[u], D[v]);
+        }
+    }
+}
