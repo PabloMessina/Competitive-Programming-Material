f866a5139d8efb04a3198f4f51663beb67ba34a4
PabloMessina <pamessina@uc.cl>
2020-10-23 02:50:10 -0300

    update

--- Solved problems/UVA/12193_Hooligan.cpp
+++ Solved problems/UVA/12193_Hooligan.cpp
@@ -1,11 +1,19 @@
-// Time Complexity:
-// - general worst case: O (|E| * |V|^2)
-// - unit capacities: O( min(V^(2/3), sqrt(E)) * E)
-// - Bipartite graph (unit capacities) + source & sink (any capacities): O(E sqrt V)
-
+// tags: max flow, Dinic
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
+// defines
+#define rep(i,a,b) for(int i = a; i < b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+#define umap unordered_map
+#define uset unordered_set
+#define ff first
+#define ss second
+// typedefs
+typedef pair<int,int> ii;
+typedef unsigned long long int ull;
 typedef long long int ll;
+// -------------------------------
 
 struct Dinic {
     struct edge {
@@ -74,9 +82,54 @@ struct Dinic {
     }
 };
 
-// usage
+umap<int,int> x2id;
+int ID = 0;
+int get_id(int x) {
+    auto it = x2id.find(x);
+    if (it == x2id.end()) return x2id[x] = ID++;
+    return it->second;
+}
 int main() {
-    Dinic din(2);
-    din.add_edge(0,1,10);
-    ll mf = din.max_flow(0,1);
+    ios::sync_with_stdio(false); cin.tie(0);
+    int N, M, G;
+    nextcase:
+    while (cin >> N >> M >> G and N) {
+        vector<int> P(N, 0);
+        vector<int> C(N * N, 0);
+        rep(_,0,G) {
+            int i, j; char c; cin >> i >> c >> j;
+            if (c == '=') P[i]++, P[j]++;
+            else if (c == '>') P[i] += 2;
+            else P[j] += 2;
+            if (i > j) swap (i, j);
+            C[i * N + j]++;
+        }
+        rep(i,1,N) P[0] += (M - C[i]) * 2;
+        rep(i,1,N) if (P[i] >= P[0]) {
+            cout << "N\n";
+            goto nextcase;
+        }
+        ID = 0;
+        x2id.clear();
+        Dinic din((N-1)*(N-2)/2 + N-1 + 2);
+        int s = get_id(888888), t = get_id(999999);
+        int tot_pts = 0;
+        rep(i,1,N) rep(j,i+1,N) {
+            int u = get_id(i * N + j);
+            int ii = get_id(N * N + i);
+            int jj = get_id(N * N + j);
+            int pts = (M - C[i * N + j])*2;
+            tot_pts += pts;
+            din.add_edge(s, u, pts);
+            din.add_edge(u, ii, pts);
+            din.add_edge(u, jj, pts);            
+        }
+        rep(i,1,N) din.add_edge(get_id(N * N + i), t, P[0] - P[i] - 1);
+        if (din.max_flow(s,t) == tot_pts) {
+            cout << "Y\n";
+        } else {
+            cout << "N\n";
+        }
+    }
+    return 0;
 }
