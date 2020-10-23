48f70e33d05f10dcceb47d1f8e590ac7fbe14dc9
PabloMessina <pamessina@uc.cl>
2020-10-23 17:18:37 -0300

    update

--- Strings/RollingHashing.cpp
+++ Strings/RollingHashing.cpp
@@ -4,41 +4,6 @@ using namespace std;
 typedef unsigned long long int ull;
 const int MAXLEN = 1e6;
 
-// -----------------------------
-// Rolling Hashing: single hash
-
-struct RH_single { // rolling hashing
-    static const ull B = 131; // base
-    static const ull P = 1e9 + 21; // prime
-    static ull pow[MAXLEN];    
-    static ull add(ull x, ull y) { return (x + y) % P; }
-    static ull mul(ull x, ull y) { return (x * y) % P; }
-    static void init() {
-        pow[0] = 1;
-        rep(i,1,MAXLEN) pow[i] = mul(B, pow[i-1]);
-    }    
-    vector<ull> h;
-    int len;
-    void init(vector<int>& s) {
-        for (int x : s) assert (x >= 1); // DEBUGGING
-        len = s.size();
-        h.resize(len);
-        h[0] = s[0];
-        rep(i,1,len) h[i] = add(mul(h[i-1], B), s[i]);
-    }
-    RH_single(vector<int>& s) { init(s); } // from vector<int>
-    RH_single(string& s, char ref) { // from string
-        vector<int> tmp; for(char c : s) tmp.push_back(c - ref + 1);
-        init(tmp);
-    }
-    ull hash(int i, int j) {
-        if (i == 0) return h[j];
-        return add(h[j], P - mul(h[i-1], pow[j - i + 1]));
-    }
-    ull hash() { return h[len-1]; }
-};
-ull RH_single::pow[MAXLEN]; // necessary for the code to compile
-
 // --------------------------------------------
 // Rolling Hashing: double hash (extra safety)
 
@@ -65,7 +30,7 @@ struct RH_double { // rolling hashing
         }
     }
     RH_double(vector<int>& s) { init(s); } // from vector<int>
-    RH_double(string& s, char ref) { // from string
+    RH_double(string& s, char ref = 'a') { // from string
         vector<int> tmp; for (char c : s) tmp.push_back(c - ref + 1);
         init(tmp);
     }
@@ -81,6 +46,41 @@ struct RH_double { // rolling hashing
 // these lines are necessary for the code to compile
 const ull RH_double::P[2] = {(ull)1e9+21, (ull)1e9+9};
 ull RH_double::pow[2][MAXLEN];
+
+// -----------------------------
+// Rolling Hashing: single hash
+
+struct RH_single { // rolling hashing
+    static const ull B = 131; // base
+    static const ull P = 1e9 + 21; // prime
+    static ull pow[MAXLEN];    
+    static ull add(ull x, ull y) { return (x + y) % P; }
+    static ull mul(ull x, ull y) { return (x * y) % P; }
+    static void init() {
+        pow[0] = 1;
+        rep(i,1,MAXLEN) pow[i] = mul(B, pow[i-1]);
+    }    
+    vector<ull> h;
+    int len;
+    void init(vector<int>& s) {
+        for (int x : s) assert (x >= 1); // DEBUGGING
+        len = s.size();
+        h.resize(len);
+        h[0] = s[0];
+        rep(i,1,len) h[i] = add(mul(h[i-1], B), s[i]);
+    }
+    RH_single(vector<int>& s) { init(s); } // from vector<int>
+    RH_single(string& s, char ref = 'a') { // from string
+        vector<int> tmp; for(char c : s) tmp.push_back(c - ref + 1);
+        init(tmp);
+    }
+    ull hash(int i, int j) {
+        if (i == 0) return h[j];
+        return add(h[j], P - mul(h[i-1], pow[j - i + 1]));
+    }
+    ull hash() { return h[len-1]; }
+};
+ull RH_single::pow[MAXLEN]; // necessary for the code to compile
 
 // ----- usage & testing
 int main() {
