5e78453f036f1c314a68faa5f9fb53d1584f51cb
PabloMessina <pamessina@uc.cl>
2020-11-15 17:50:24 -0300

    update

--- Solved problems/Codeforces/1285D_Dr.EvilUnderscores.cpp
+++ Solved problems/Codeforces/1285D_Dr.EvilUnderscores.cpp
@@ -2,14 +2,7 @@
 #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
-// defines
 #define rep(i,a,b) for(int i = a; i < b; ++i)
-#define invrep(i,b,a) for(int i = b; i >= a; --i)
-#define umap unordered_map
-#define uset unordered_set
-// typedefs
-typedef unsigned long long int ull;
-typedef long long int ll;
 // -------------------------------
 struct Trie {
     vector<vector<int>> g;
@@ -32,23 +25,15 @@ struct Trie {
         count[v]++;
         return v;
     }
-    void insert(const string& s, char ref = 'a') {  // insert string
-        int u = 0; for (char c : s) u = move_to(u, c - ref);
-    }    
-    void insert(vector<int>& s) { // insert vector<int>
+    void insert(vector<int>& s) {
         int u = 0; for (int c : s) u = move_to(u, c);
     }
     int size() { return g.size(); }
-
     int min_xor(int u, int i) {
         bool zero = g[u][0] != -1;
         bool one = g[u][1] != -1;
-        if (zero and not one) {
-            return min_xor(g[u][0], i+1);
-        }
-        if (one and not zero) {
-            return min_xor(g[u][1], i+1);
-        }
+        if (zero and not one) return min_xor(g[u][0], i+1);
+        if (one and not zero) return min_xor(g[u][1], i+1);
         if (one and zero) {
             return min(min_xor(g[u][0], i+1), min_xor(g[u][1], i+1)) | (1 << (29 - i)) ;
         }
@@ -57,16 +42,13 @@ struct Trie {
 };
 
 int main() {
-    ios::sync_with_stdio(false); 
-    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false); cin.tie(0);
     int n; cin >> n;
     Trie trie(2);
     rep(i,0,n) {
         int x; cin >> x;
         vector<int> bits(30);
-        rep(i,0,30) {
-            bits[i] = (x >> (30 - i - 1)) & 1;
-        }
+        rep(i,0,30) bits[i] = (x >> (30 - i - 1)) & 1;
         trie.insert(bits);
     }
     cout << trie.min_xor(0, 0) << '\n';
