f41584f9d75993320abccb90c6d0ae1d98e9468c
PabloMessina <pamessina@uc.cl>
2020-11-27 13:33:58 -0300

    update

--- Solved problems/LiveArchive/7207_ExposingCorruption.cpp
+++ Solved problems/LiveArchive/7207_ExposingCorruption.cpp
@@ -1,20 +1,17 @@
 // tags: BFS, preprocessing, DP, bottom-up, Knapsack
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,a,b) for(int i=a; i<=b; ++i)
-#define invrep(i,a,b) for(int i=a; i>=b; --i)
-#define MAXN 200
-#define MAXB 10000
-
-typedef vector<int> vi;
-
+#define rep(i,a,b) for(int i = a; i < b; ++i)
+#define invrep(i,b,a) for(int i = b; i >= a; --i)
+// -------------------------------
+const int MAXN = 200;
+const int MAXB = 10000;
 int D, P, R, B;
 int price[MAXN];
 int party[MAXN];
-vector<vi> g;
-
+vector<vector<int>> g;
 int CN;
-
 int idx;
 int gain[MAXN][2];
 int weight[MAXN];
@@ -26,10 +23,8 @@ void bfs(int s) {
     gain[idx][1-party[s]] = 1;
     weight[idx] = price[s];
     vis[s] = true;
-
     while (!q.empty()) {
         int u = q.front(); q.pop();
-
         for (int v : g[u]) {
             if (!vis[v]) {
                 vis[v] = true;
@@ -48,12 +43,12 @@ int dp(int p) {
     // first item
     memset(memo, 0, sizeof(memo[0]) * (B+1));
     if (gain[0][p] > 0) {
-        rep (b, weight[0], B) {
+        rep(b,weight[0],B+1) {
             memo[b] = gain[0][p];
         }
     }
     // other items
-    rep (i, 1, CN-1) {
+    rep(i,1,CN) {
         if (gain[i][p] > 0) {
             invrep(b, B, weight[i]) {
                 memo[b] = max(memo[b], gain[i][p] + memo[b - weight[i]]);
@@ -64,34 +59,22 @@ int dp(int p) {
 }
 
 int main() {
-    while(scanf("%d%d%d%d", &D, &P, &R, &B) == 4) {
+    ios::sync_with_stdio(false); cin.tie(0);
+    while(cin >> D >> P >> R >> B) {
         int n = D+P;
-        g.assign(n, vi());
-
-        rep(i, 0, D-1) {
-            scanf("%d", &price[i]);
-            party[i] = 0;
-        }
-        rep(i, 0, P-1) {
-            scanf("%d", &price[i+D]);
-            party[i+D] = 1;
-        }
-        rep(i,1,R) {
-            int x, y; scanf("%d%d", &x,&y); --x, --y;
+        g.assign(n, vector<int>());
+        rep(i,0,D) { cin >> price[i]; party[i] = 0; }
+        rep(i,0,P) { cin >> price[i+D]; party[i+D] = 1; }
+        rep(_,0,R) {
+            int x, y; cin >> x >> y; --x, --y;
             g[x].push_back(D+y);
             g[D+y].push_back(x);
         }
-        //
         memset(vis, 0, sizeof(vis[0]) * n);
         idx = 0;
-        for (int i = 0; i < n; ++i) {
-            if (!vis[i]) {
-                bfs(i); idx++;
-            }
-        }
+        rep(i,0,n) if (!vis[i]) bfs(i), idx++;
         CN = idx;
-        //
-        printf("%d %d\n", D + dp(0), P + dp(1));
+        cout << D + dp(0) << " " << P + dp(1) << '\n';
     }
     return 0;
 }
