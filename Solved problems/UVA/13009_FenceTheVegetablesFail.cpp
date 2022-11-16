a7099eb64f4b441605c827466b98ae15da102aa2
PabloMessina <pamessina@uc.cl>
2022-11-16 08:06:23 -0300

    update

--- Solved problems/UVA/13009_FenceTheVegetablesFail.cpp
+++ Solved problems/UVA/13009_FenceTheVegetablesFail.cpp
@@ -20,16 +20,16 @@ typedef long long int ll;
 
 int P, V;
 struct Point {
-	ll x, y;
-	Point(ll x, ll y) : x(x), y(y) {}
+    ll x, y;
+    Point(ll x, ll y) : x(x), y(y) {}
 };
 
 vector<Point> plants, fence;
 struct Event {
-	ll x, y1, y2;
-	int idx;
-	bool isPlant;
-	Event(ll x, ll y1, ll y2, int idx, bool isPlant) : x(x), y1(y1), y2(y2), idx(idx), isPlant(isPlant) {}
+    ll x, y1, y2;
+    int idx;
+    bool isPlant;
+    Event(ll x, ll y1, ll y2, int idx, bool isPlant) : x(x), y1(y1), y2(y2), idx(idx), isPlant(isPlant) {}
 };
 
 struct FT { // fenwick tree
@@ -49,71 +49,71 @@ struct FT { // fenwick tree
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
-	while (cin >> P >> V) { // for each input case
-		set<ll> yset;
-		rep(i,0,P) { // read plant coordinates
-			ll x, y; cin >> x >> y;
-			plants.emplace_back(x, y);
-			yset.insert(y);
-		}
-		rep(i,0,V) { // read fence's vertices coordinates
-			ll x, y; cin >> x >> y;
-			fence.emplace_back(x, y);
-			yset.insert(y);
-		}
-		// create events
-		vector<Event> events;
-		// add plant events
-		rep(i,0,P) {
-			ll x = plants[i].x;
-			ll y = plants[i].y;
-			events.emplace_back(x, y, y, i, true);
-		}
-		// add segment events
-		rep(i,0,V) {
-			int j = (i+1) % V;
-			if (fence[i].x == fence[j].x) { // vertical segment
-				assert (fence[i].y != fence[j].y);
-				ll miny = fence[i].y;
-				ll maxy = fence[j].y;
-				if (miny > maxy) swap(miny, maxy);
-				events.emplace_back(fence[i].x, miny, maxy, -1, false);
-			}
-		}
-		// sort events by x coordinate
-		sort(events.begin(), events.end(), [](Event& a, Event& b) {
-			return a.x < b.x;
-		});
-		// domain compression: map all y values to a narrower domain
-		umap<ll,ll> y2idx;
-		int idx = 1;
-		for (ll y : yset) y2idx[y] = idx++;
-		// run sweep line: left to right
-		FT ft(y2idx.size()+5);
-		ll ans = 0;
-		for (Event& e : events) {
-			if (e.isPlant) { // plant encountered
-				int idx = y2idx[e.y1];
-				// if outside fence -> add plant's value to sum
-				if (ft.query(idx) == 0) ans += e.idx + 1;
-			} else { // segment encountered
-				int a = y2idx[e.y1];
-				int b = y2idx[e.y2];
-				assert(a < b);
-				if (ft.query(a) == 0) { // nothing in range
-					// add 1 to range -> we are now inside the fence
-					ft.update(a, b-1, 1);
-				} else {
-					// substract 1 to range -> we are now outside the fence
-					ft.update(a, b-1, -1);
-				}
-			}
-		}
-		// print answer
-		cout << ans << '\n';
-		// clear memory
-		plants.clear();
-		fence.clear();
-	}
+    while (cin >> P >> V) { // for each input case
+        set<ll> yset;
+        rep(i,0,P) { // read plant coordinates
+            ll x, y; cin >> x >> y;
+            plants.emplace_back(x, y);
+            yset.insert(y);
+        }
+        rep(i,0,V) { // read fence's vertices coordinates
+            ll x, y; cin >> x >> y;
+            fence.emplace_back(x, y);
+            yset.insert(y);
+        }
+        // create events
+        vector<Event> events;
+        // add plant events
+        rep(i,0,P) {
+            ll x = plants[i].x;
+            ll y = plants[i].y;
+            events.emplace_back(x, y, y, i, true);
+        }
+        // add segment events
+        rep(i,0,V) {
+            int j = (i+1) % V;
+            if (fence[i].x == fence[j].x) { // vertical segment
+                assert (fence[i].y != fence[j].y);
+                ll miny = fence[i].y;
+                ll maxy = fence[j].y;
+                if (miny > maxy) swap(miny, maxy);
+                events.emplace_back(fence[i].x, miny, maxy, -1, false);
+            }
+        }
+        // sort events by x coordinate
+        sort(events.begin(), events.end(), [](Event& a, Event& b) {
+            return a.x < b.x;
+        });
+        // domain compression: map all y values to a narrower domain
+        umap<ll,ll> y2idx;
+        int idx = 1;
+        for (ll y : yset) y2idx[y] = idx++;
+        // run sweep line: left to right
+        FT ft(y2idx.size()+5);
+        ll ans = 0;
+        for (Event& e : events) {
+            if (e.isPlant) { // plant encountered
+                int idx = y2idx[e.y1];
+                // if outside fence -> add plant's value to sum
+                if (ft.query(idx) == 0) ans += e.idx + 1;
+            } else { // segment encountered
+                int a = y2idx[e.y1];
+                int b = y2idx[e.y2];
+                assert(a < b);
+                if (ft.query(a) == 0) { // nothing in range
+                    // add 1 to range -> we are now inside the fence
+                    ft.update(a, b-1, 1);
+                } else {
+                    // substract 1 to range -> we are now outside the fence
+                    ft.update(a, b-1, -1);
+                }
+            }
+        }
+        // print answer
+        cout << ans << '\n';
+        // clear memory
+        plants.clear();
+        fence.clear();
+    }
     return 0;
 }
